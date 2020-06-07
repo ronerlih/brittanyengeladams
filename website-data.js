c85d74f9231dc9732c58c881aaa74067c4f1bad8
ron erlih <ronerlih@gmail.com>
2020-06-07 18:24:52 -0400

    ording with dooview

--- website-data.js
+++ website-data.js
@@ -1,223 +1,162 @@
 let WEBSITE_DATA = [
-	{	
+	{
 		name: "Okwui Okpokwasili 'Sitting on a Man's Head'",
 		img: "images/press.png",
 		blurb: "Performances at Danspace St. Marks Church 2020",
 		link: "https://danspaceproject.org/2020/01/16/platform-2020//",
-		credit: "photo: Sasha Arutyunova"
-	},			{	
-	name: "Annie-B Parson – Big Dance Theater 'Mood Room'",
-	img: "images/AnnieBMoodRoom.png",
-	blurb: "Residency at Atlantic Center for the Arts 2020",
-	link: "https://atlanticcenterforthearts.org/master-artist/annie-b-parson/",
-	credit: "photo: Annie-B Parson"
-},
-
-{	
-	name: "Lars Jan's 'The White Album'",
-	img: "images/the-white-album-by-joan-didion-lars-jan-early-morning-opera-2.jpg",
-	blurb: "Performances at Sydney Festival 2020",
-	link: "https://www.sydneyfestival.org.au/events/joan-didions-the-white-album",
-	credit: "photo: Reed Hutchinson"
-},
-	
-	{	
-	name: "Yvonne Rainer's 'Mattress Dance'",
-	img: "images/merlin_164267745_734502e2-bf7a-4c96-9c6c-e6d1bd0948e1-jumbo.jpg",
-	blurb: "Performa Festival 2019",
-	link: "https://www.nytimes.com/2019/11/12/arts/dance/yvonne-rainer-parts-of-some-sextets-performa.html",
-	credit: "photo: Adam Kremer"
-},
-{	
-	name: "Emily Coates' 'Ode to Oskar Schlemmer'",
-	img: "images/Document-Journal_Oskar_2.jpg",
-	blurb: "Performa Gala 2019",
-	link: "https://www.documentjournal.com/2019/11/performa-revives-the-legacy-of-oskar-schlemmer-the-bauhauss-avant-garde-theater-visionary/",
-	credit: "photo: BFA"
-},
-{	
-	name: "Bates College 'Horton Workshop'",
-	img: "images/67330581_10156432691254080_503553308791668736_o.jpg",
-	blurb: "Bates Summer Dance Festival 2019",
-	link: "https://www.batesdancefestival.org/about/faculty/",
-	credit: "photo: BDF"
-},
-{	
-	name: "Cunningham Centennial 'Conversations with Merce'",
-	img: "images/59694604_10156421603797725_8517050113678376960_n.jpg",
-	blurb: "Choreography by Netta Yerushalmy/Performances at Skirball 2019",
-	link: "https://nyuskirball.org/events/cunningham-centennial/?fbclid=IwAR2pqxII4coGFNkUJmEePC7d_IG945wamkUJS1uG8vFxXPVJYdHeuoqyy2U",
-	credit: "photo: Ian Douglas"
-},
-{	
-	name: "Guggenheim Museum 'Works & Process'",
-	img: "images/MichelleTabnick.jpg",
-	blurb: "Choreography by Netta Yerushalmy/Designs by Reid Bartelme and Harriet Jung 2019",
-	link: "https://www.guggenheim.org/event/event_series/works-process",
-	credit: "photo: Michelle Tabnick"
-},
-{	
-	name: "Lars Jan's 'The White Album'",
-	img: "images/the-white-album-by-joan-didion-lars-jan-early-morning-opera-5.jpg",
-	blurb: "Performances at UCLA 2019.",
-	link: "https://arts.ucla.edu/single/the-white-album-by-joan-didion/",
-	credit: "photo: Reed Hutchinson "
-},
-{	
-	name: "Netta Yerushalmy's 'Paramodernities'",
-	img: "images/merlin_142057125_728b3c73-ff0a-498e-a3c0-b5dd2ef50857-jumbo.jpg",
-	blurb: "NYC premiere at New York Live Arts 2018",
-	link: "https://newyorklivearts.org/event/paramodernities/",
-	credit: "photo: Andrew Spear"
-},
-
-{	name: "Donna Uchizono Company's 'March Under an Empty Reign'",
-	img: "images/10_Photo_by_Julieta_Cervantes_-_Hristoula_Hrakas_Julie_Alexander_Antonio_Ramos_smaller_sizeevent_image.jpg",
-	blurb: "Performances at the Joyce Theater for Quadrille Festival 2018 ",
-	link: "https://www.joyce.org/performances/donna-uchizono-company",
-	credit: "photo: Julieta Cervantes"
-},
-
-{	name: "Donna Uchizono Company's 'Iron Jane'",
-	img: "images/Z44A4085_normal.jpg",
-	blurb: "Residency at Maggie Allesee National Center for Choreography 2018",
-	link: "http://www.mancc.org/artists/donna-uchizono/",
-	credit: "photo: Chris Cameron"
-},
-
-
-{	name: "Netta Yerushalmy's 'Paramodernities'",
-	img: "images/merlin_142057272_a312d7a4-4768-432a-b8f2-fb8b93a6b3d7-articleLarge.jpg",
-	blurb: "Encyclopedia Premiere at Jacobs Pillow 2017",
-	link: "https://www.jacobspillow.org/events/paramodernities-netta-yerushalmy/",
-	credit: "photo: Andrew Spear"
-},
-{	name: "Netta Yerushalmy",
-	img: "images/NDPP.jpg",
-	blurb: "BAC story by Neil Greenberg",
-	link: "https://bacnyc.org/residencies/resident/netta-yerushalmy1",
-	credit: "photo: Janelle Jones"
-},
-{	
-	name: "River To River Festival (Press)",
-	img: "images/women-enter.jpg",
-	blurb: "Review of Paramodernities #3 at the Museum of the American Indian",
-	link: "https://www.culturebot.org/2017/09/27419/dancing-a-history-and-defining-a-project-some-notes-on-netta-yerushalmys-paramodernities-3/",
-	credit: "photo: Paula Lobo"
-},
-
-{	name: "ODC Performance (Press)",
-	img: "images/nyla-paramodernities.jpg",
-	blurb: "Review of PM #3&4 at ODC in San Francisco",
-	link: "https://culturevulture.net/dance/paramodernities-west-coast-premiere",
-	credit: "photo: Andrew Spear"
-},
-
-{	name: "Open Space San Francisco MOMA (Press)",
-	img: "images/christopherduggan2.png",
-	blurb: "Review of PM #3&4",
-	link: "https://openspace.sfmoma.org/2018/04/lineage-mimicry-and-ambivalence/",
-	credit: "photo: Christopher Duggan"
-},
-
-
-
-// {	name: "New York Times instagram",
-// 	img: "images/10instagram-facebookJumbo.jpg",
-// 	blurb: "Ny Times video of Madison Square Park residency",
-// 	link: "https://www.nytimes.com/2017/08/10/arts/dance/netta-yerushalmy-paramodernities-madison-square-park.html",
-// 	credit: "photo: Adam Golfer"
-// },
-
-// {	name: "LMCC Gala 2017",
-// 	img: "images/gala.webp",
-// 	blurb: "Performance at the 2017 LMCC annual fundraising Downtown Dinner Gala",
-// 	link: "https://www.broadwayworld.com/article/LMCCs-Downtown-Dinner-2017-to-Honor-Durst-Family-Keith-Haring-Foundation-and-More-20170406",
-// 	credit: ""
-// },
-	
-{	name: "Spike Lee's 'Chiraq'",
-	img: "images/Chiraq-Poster-UK.jpg",
-	blurb: "Featured dancer in film 2016",
-	link: "",
-	credit: ""
-},
-	
-{	name: "Punchdrunk 'Sleep No more'",
-	img: "images/tn-500_giafrese-0304.jpg",
-	blurb: "Performeances at the McKittrick Hotel 2015",
-	link: "https://www.broadwayworld.com/article/Photo-Flash-Inside-McKittrick-Hotels-New-Years-Eve-Party-THE-KINGS-WINTER-MASQUERADE-20150102",
-	credit: "SNM photo"
-},
-
-{	name: "Orit Ben Shitrit's 'A ward of feral horses'",
-	img: "images/OritBenShitrit_Dance.jpg",
-	blurb: "EMPAC commissioned dance film 2014",
-	link: "http://empac.rpi.edu/events/2015/spring/dance-screen/ward-feral-horses.pdf",
-	credit: "photo: Orit Ben-Shitrit"
-},
-
-{	name: "The Great Gatsby 'Movie Premiere'",
-	img: "images/the-great-gatsby-girls.jpg",
-	blurb: "Performances at the Great Gatsby Movie Premiere in NYC 2013",
-	link: "https://www.businessinsider.com/the-great-gatsby-new-york-premiere-and-party-2013-5",
-	credit: ""
-},
-
-// {	name: "The Forward interview",
-// 	img: "images/Ailey_II_s_Brittany_Engel-Adams._Photo_by_Eduardo_Patino_NYC-prv.jpg",
-// 	blurb: "Dance in Israel Q and A",
-// 	link: "https://forward.com/schmooze/150294/q-and-a-brittany-engel-adams-on-dance-and-israel/"
-// },
-
-// {	name: "PM at Roger Williams University",
-// 	img: "images/20065885_458129447883975_4156221487059042304_n.jpg",
-// 	blurb: "Performance of PM #4 choreography by Netta Yerushalmy 2018",
-// 	link: "https://www.rwu.edu/events/alive-arts-series-paramodernities-netta-yerushalmy",
-// 	credit: "photo: Janelle Jones"
-// },
-
-{	name: "Dana Katz's 'Prospect Minds'",
-	img: "images/69604_557193287654529_1668116553_n.jpg",
-	blurb: "Performances at the 92nd Street Y 2013",
-	link: "https://vimeo.com/58922736",
-	credit: "photo: Dana Katz"
-},
-
-{	name: "HBO television series 'Boardwalk Empire'",
-	img: "images/onyx-dancers.jpg",
-	blurb: "Co-star in Season 4 2013",
-	link: "http://boardwalkempire.wikia.com/wiki/Brittany_Engel-Adams",
-	credit: "HBO photo"
-},
-
-{	name: "Naomi Goldberg Hass's Dances for a Variable Population '10027'",
-	img: "images/RebeccaHiteTeicheira.jpg",
-	blurb: "Site-specific Performances in Manhattan 2015",
-	link: "https://www.dvpnyc.org/10027-everything-you-need-to-know/",
-	credit: "photo: Talya Charef"
-},
-
-{	name: "Stefanie Batten Bland's 'Patient(ce)",
-	img: "images/New-Music-Thank-You-resize.jpg",
-	blurb: "Performances at Harlem Stage Gatehouse 2015",
-	link: "https://www.dance-enthusiast.com/features/view/SBB-15-Requiem",
-	credit: "photo: Mark Millman"
-},
-
-{	name: "Stefanie Batten Bland's 'A Place of Sun'",
-	img: "images/RubyWashington.jpg",
-	blurb: "Residency and Performance at Baryshnikov Arts Center 2012",
-	link: "https://www.nytimes.com/2012/05/22/arts/dance/stefanie-batten-blands-place-of-sun-at-baryshnikov.html",
-	credit: "photo: Rubby Washington"
+		credit: "photo: Sasha Arutyunova",
+	},
+	{
+		name: "Annie-B Parson – Big Dance Theater 'Mood Room'",
+		img: "images/AnnieBMoodRoom.png",
+		blurb: "Residency at Atlantic Center for the Arts 2020",
+		link: "https://atlanticcenterforthearts.org/master-artist/annie-b-parson/",
+		credit: "photo: Annie-B Parson",
 	},
 
-{	name: "Ailey II Company under the direction of Sylvia Waters",
-	img: "images/Ailey_II_in_Thang_Dao_s_Echoes_Photo_by_Eduardo_Patino.jpg",
-	blurb: "Performances and Community Outreach 2010",
-	link: "http://pressroom.alvinailey.org/file?fid=5711a33adfc2a546ab14fe4e",
-	credit: "photo: Eduardo Patino"
-}
+	{
+		name: "Lars Jan's 'The White Album'",
+		img: "images/the-white-album-by-joan-didion-lars-jan-early-morning-opera-2.jpg",
+		blurb: "Performances at Sydney Festival 2020",
+		link: "https://www.sydneyfestival.org.au/events/joan-didions-the-white-album",
+		credit: "photo: Reed Hutchinson",
+	},
 
+	{
+		name: "Yvonne Rainer's 'Mattress Dance'",
+		img: "images/merlin_164267745_734502e2-bf7a-4c96-9c6c-e6d1bd0948e1-jumbo.jpg",
+		blurb: "Performa Festival 2019",
+		link: "https://www.nytimes.com/2019/11/12/arts/dance/yvonne-rainer-parts-of-some-sextets-performa.html",
+		credit: "photo: Adam Kremer",
+	},
+	{
+		name: "Emily Coates' 'Ode to Oskar Schlemmer'",
+		img: "images/Document-Journal_Oskar_2.jpg",
+		blurb: "Performa Gala 2019",
+		link: "https://www.documentjournal.com/2019/11/performa-revives-the-legacy-of-oskar-schlemmer-the-bauhauss-avant-garde-theater-visionary/",
+		credit: "photo: BFA",
+	},
+	{
+		name: "Bates College 'Horton Workshop'",
+		img: "images/67330581_10156432691254080_503553308791668736_o.jpg",
+		blurb: "Bates Summer Dance Festival 2019",
+		link: "https://www.batesdancefestival.org/about/faculty/",
+		credit: "photo: BDF",
+	},
+	{
+		name: "Cunningham Centennial 'Conversations with Merce'",
+		img: "images/59694604_10156421603797725_8517050113678376960_n.jpg",
+		blurb: "Choreography by Netta Yerushalmy/Performances at Skirball 2019",
+		link: "https://nyuskirball.org/events/cunningham-centennial/?fbclid=IwAR2pqxII4coGFNkUJmEePC7d_IG945wamkUJS1uG8vFxXPVJYdHeuoqyy2U",
+		credit: "photo: Ian Douglas",
+	},
+	{
+		name: "Guggenheim Museum 'Works & Process'",
+		img: "images/MichelleTabnick.jpg",
+		blurb: "Choreography by Netta Yerushalmy/Designs by Reid Bartelme and Harriet Jung 2019",
+		link: "https://www.guggenheim.org/event/event_series/works-process",
+		credit: "photo: Michelle Tabnick",
+	},
+	{
+		name: "Lars Jan's 'The White Album'",
+		img: "images/the-white-album-by-joan-didion-lars-jan-early-morning-opera-5.jpg",
+		blurb: "Performances at UCLA 2019.",
+		link: "https://arts.ucla.edu/single/the-white-album-by-joan-didion/",
+		credit: "photo: Reed Hutchinson ",
+	},
+	{
+		name: "Netta Yerushalmy's 'Paramodernities'",
+		img: "images/merlin_142057125_728b3c73-ff0a-498e-a3c0-b5dd2ef50857-jumbo.jpg",
+		blurb: "NYC premiere at New York Live Arts 2018",
+		link: "https://newyorklivearts.org/event/paramodernities/",
+		credit: "photo: Andrew Spear",
+	},
 
-]
+	{
+		name: "Donna Uchizono Company's 'March Under an Empty Reign'",
+		img: "images/10_Photo_by_Julieta_Cervantes_-_Hristoula_Hrakas_Julie_Alexander_Antonio_Ramos_smaller_sizeevent_image.jpg",
+		blurb: "Performances at the Joyce Theater for Quadrille Festival 2018 ",
+		link: "https://www.joyce.org/performances/donna-uchizono-company",
+		credit: "photo: Julieta Cervantes",
+	},
+
+	{ name: "Donna Uchizono Company's 'Iron Jane'", img: "images/Z44A4085_normal.jpg", blurb: "Residency at Maggie Allesee National Center for Choreography 2018", link: "http://www.mancc.org/artists/donna-uchizono/", credit: "photo: Chris Cameron" },
+
+	{
+		name: "Netta Yerushalmy's 'Paramodernities'",
+		img: "images/merlin_142057272_a312d7a4-4768-432a-b8f2-fb8b93a6b3d7-articleLarge.jpg",
+		blurb: "Encyclopedia Premiere at Jacobs Pillow 2017",
+		link: "https://www.jacobspillow.org/events/paramodernities-netta-yerushalmy/",
+		credit: "photo: Andrew Spear",
+	},
+	{ name: "ODC Performance (Press)", img: "images/nyla-paramodernities.jpg", blurb: "Review of PM #3&4 at ODC in San Francisco", link: "https://culturevulture.net/dance/paramodernities-west-coast-premiere", credit: "photo: Andrew Spear" },
+	{ name: "Netta Yerushalmy", img: "images/NDPP.jpg", blurb: "BAC story by Neil Greenberg", link: "https://bacnyc.org/residencies/resident/netta-yerushalmy1", credit: "photo: Janelle Jones" },
+	{
+		name: "River To River Festival (Press)",
+		img: "images/women-enter.jpg",
+		blurb: "Review of Paramodernities #3 at the Museum of the American Indian",
+		link: "https://www.culturebot.org/2017/09/27419/dancing-a-history-and-defining-a-project-some-notes-on-netta-yerushalmys-paramodernities-3/",
+		credit: "photo: Paula Lobo",
+	},
+
+	{ name: "Open Space San Francisco MOMA (Press)", img: "images/christopherduggan2.png", blurb: "Review of PM #3&4", link: "https://openspace.sfmoma.org/2018/04/lineage-mimicry-and-ambivalence/", credit: "photo: Christopher Duggan" },
+
+	// {	name: "New York Times instagram",
+	// 	img: "images/10instagram-facebookJumbo.jpg",
+	// 	blurb: "Ny Times video of Madison Square Park residency",
+	// 	link: "https://www.nytimes.com/2017/08/10/arts/dance/netta-yerushalmy-paramodernities-madison-square-park.html",
+	// 	credit: "photo: Adam Golfer"
+	// },
+
+	// {	name: "LMCC Gala 2017",
+	// 	img: "images/gala.webp",
+	// 	blurb: "Performance at the 2017 LMCC annual fundraising Downtown Dinner Gala",
+	// 	link: "https://www.broadwayworld.com/article/LMCCs-Downtown-Dinner-2017-to-Honor-Durst-Family-Keith-Haring-Foundation-and-More-20170406",
+	// 	credit: ""
+	// },
+
+	{ name: "Spike Lee's 'Chiraq'", img: "images/Chiraq-Poster-UK.jpg", blurb: "Featured dancer in film 2016", link: "", credit: "" },
+
+	{ name: "Punchdrunk 'Sleep No more'", img: "images/tn-500_giafrese-0304.jpg", blurb: "Performeances at the McKittrick Hotel 2015", link: "https://www.broadwayworld.com/article/Photo-Flash-Inside-McKittrick-Hotels-New-Years-Eve-Party-THE-KINGS-WINTER-MASQUERADE-20150102", credit: "SNM photo" },
+
+	{ name: "Stefanie Batten Bland's 'Patient(ce)", img: "images/New-Music-Thank-You-resize.jpg", blurb: "Performances at Harlem Stage Gatehouse 2015", link: "https://www.dance-enthusiast.com/features/view/SBB-15-Requiem", credit: "photo: Mark Millman" },
+
+	{ name: "Naomi Goldberg Hass's Dances for a Variable Population '10027'", img: "images/RebeccaHiteTeicheira.jpg", blurb: "Site-specific Performances in Manhattan 2015", link: "https://www.dvpnyc.org/10027-everything-you-need-to-know/", credit: "photo: Talya Charef" },
+
+	{ name: "Orit Ben Shitrit's 'A ward of feral horses'", img: "images/OritBenShitrit_Dance.jpg", blurb: "EMPAC commissioned dance film 2014", link: "http://empac.rpi.edu/events/2015/spring/dance-screen/ward-feral-horses.pdf", credit: "photo: Orit Ben-Shitrit" },
+
+	{ name: "The Great Gatsby 'Movie Premiere'", img: "images/the-great-gatsby-girls.jpg", blurb: "Performances at the Great Gatsby Movie Premiere in NYC 2013", link: "https://www.businessinsider.com/the-great-gatsby-new-york-premiere-and-party-2013-5", credit: "" },
+
+	{ name: "HBO television series 'Boardwalk Empire'", img: "images/onyx-dancers.jpg", blurb: "Co-star in Season 4 2013", link: "http://boardwalkempire.wikia.com/wiki/Brittany_Engel-Adams", credit: "HBO photo" },
+
+	// {	name: "The Forward interview",
+	// 	img: "images/Ailey_II_s_Brittany_Engel-Adams._Photo_by_Eduardo_Patino_NYC-prv.jpg",
+	// 	blurb: "Dance in Israel Q and A",
+	// 	link: "https://forward.com/schmooze/150294/q-and-a-brittany-engel-adams-on-dance-and-israel/"
+	// },
+
+	// {	name: "PM at Roger Williams University",
+	// 	img: "images/20065885_458129447883975_4156221487059042304_n.jpg",
+	// 	blurb: "Performance of PM #4 choreography by Netta Yerushalmy 2018",
+	// 	link: "https://www.rwu.edu/events/alive-arts-series-paramodernities-netta-yerushalmy",
+	// 	credit: "photo: Janelle Jones"
+	// },
+
+	{ name: "Dana Katz's 'Prospect Minds'", img: "images/69604_557193287654529_1668116553_n.jpg", blurb: "Performances at the 92nd Street Y 2013", link: "https://vimeo.com/58922736", credit: "photo: Dana Katz" },
+
+	{
+		name: "Stefanie Batten Bland's 'A Place of Sun'",
+		img: "images/RubyWashington.jpg",
+		blurb: "Residency and Performance at Baryshnikov Arts Center 2012",
+		link: "https://www.nytimes.com/2012/05/22/arts/dance/stefanie-batten-blands-place-of-sun-at-baryshnikov.html",
+		credit: "photo: Rubby Washington",
+	},
+
+	{
+		name: "Ailey II Company under the direction of Sylvia Waters",
+		img: "images/Ailey_II_in_Thang_Dao_s_Echoes_Photo_by_Eduardo_Patino.jpg",
+		blurb: "Performances and Community Outreach 2010",
+		link: "http://pressroom.alvinailey.org/file?fid=5711a33adfc2a546ab14fe4e",
+		credit: "photo: Eduardo Patino",
+	},
+];
