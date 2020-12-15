3d0f47c46dd767f1b8de6474f9790d44196305b9
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2020-12-14 22:06:37 -0500

    Update website-data.js
    tanowitz

--- website-data.js
+++ website-data.js
@@ -55,7 +55,7 @@ let WEBSITE_DATA = [
 		credit: ""
 	},
 			{
-		name: "'Pam Tanowitz Dance' The Joyce Theater with a live broadcast performance event featuring Gustave Le Gray, No. 2 with music by Caroline Adelaideshaw, and a new site-specific work “Finally Unfinished: Part 1 with music by Hed tearne.",
+		name: "'Pam Tanowitz Dance' The Joyce Theater with a live broadcast performance event.",
 		img: "images/pamlive.jpg",
 		blurb: "Created in collaboration with her dancers while in quarantine via Zoom, this new work is the second iteration of Tanowitz’s site-specific dances and will take place throughout the empty Joyce Theater. Tanowitz invites audience members to engage further with an interactive companion piece created in collaboration with director Jeremy Jacob. This interactive website, specially crafted for audience members, acts as a curio cabinet that weaves and juxtaposes images, writings, videos, and other inspirations from Tanowitz's creative process, and the unique costume design by Reid and Harriet. ",
 		link: "https://www.joyce.org/pam-tanowitz-dance",
