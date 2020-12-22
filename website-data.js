897bc11f639cdfba04711ee83a0a36a64d3239b7
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2020-12-22 02:16:28 +0200

    Update website-data.js
    pat photo

--- website-data.js
+++ website-data.js
@@ -111,7 +111,7 @@ let WEBSITE_DATA = [
 
 		{
 		name: "Pat Catterson' 'Project 114: Many in one'",
-		img: "images/patcatterson.png",
+		img: "images/PATCat.png",
 		blurb: "online presentation with dancers from the US and Abroad July 2020",
 		link: "https://www.youtube.com/watch?v=ofOgXjjHz9U&list=PLD_RI35fj9t2VF--VSpYFWu-UfXQuJ1rj&index=9",
 		credit: "",
