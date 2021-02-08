fa85933bdf292d21ffd77bafb439bbabac00a319
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2021-02-08 19:45:36 +0200

    Update website-data.js
    ok

--- website-data.js
+++ website-data.js
@@ -178,7 +178,7 @@ let WEBSITE_DATA = [
 	{
 		name: "Bates College 'Horton Workshop'",
 		img: "images/Bates2inside.jpeg",
-		blurb: "Bates Summer Dance Festival 2019. Horton-based technique and experimental approach to choreographic process. Using Horton vocabulary and building choreographic operations (or scores), forming counter-modes/moments that's destabilizing the practice, and challenging constructs of modern dance and its limited notions of how the body should move.",
+		blurb: "Bates Summer Dance Festival 2019. Horton-based technique and experimental approach to choreographic process. Using Horton vocabulary and building choreographic operations (or scores), forming counter-modes/moments that are destabilizing the practice, and also challenging constructs of modern dance and its limited notions of how the body should move.",
 		credit: "photo: BDF",
 	},
 	
