3ee3b487595a5a351761e9bf2301d9b1046150d8
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2021-08-18 16:33:01 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -2,7 +2,7 @@ let WEBSITE_DATA = [
 	{
 		name: "Architecture of Memory:",
 		img: "images/HIGHRESx602copy3.png",
-		blurb: "Brittany Engel-Adams' research-project and choreography of recollections, “Architecture of Memory’” brings together personal narrative and historical accounts, using Kinect, a retrofitted game console controller and AI Machine learning technology, as an aperture with which to draw connections between past, present and future.",
+		blurb: "Brittany Engel-Adams' research-project and choreography of recollections, 'Architecture of Memory’ brings together personal narrative and historical accounts, using Kinect-a retrofitted game console controller, and AI Machine learning technology, as an aperture with which to view unforeseeable links between past, present and future. Publication forthcoming 2021, Movement Research Performance Journal, Issue #55.",
 		link: "https://movementresearch.org/publications/performance-journal/issue-55",
 		credit: "3d scan by Brittany Engel-Adams"
 	},
