23975c3d66ca7548544ee4303d87ce4331d7b7f2
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2021-09-11 18:55:22 -0400

    bard

--- website-data.js
+++ website-data.js
@@ -81,7 +81,13 @@ let WEBSITE_DATA = [
 		credit: ""
 	},
 
-	
+		{
+		name: "Pam Tanowitz Dance -- Bard Fischer SummerScape Dance Festival (2021):",
+		img: "images/capecodview.jpg",
+		blurb: "Pam Tanowitz and Jesse Montgomery’s collaboration “I was waiting for the echo of a better day” World Premiere at Fischer Bard, SummerScape Dance Festival July, 2021.",
+		link: "https://provincetownindependent.org/arts-minds/2021/02/03/wellfleets-modern-houses-are-still-laboratories-for-collaboration/",
+		credit: "photo: Maria Baranova",
+	},
 	
 		
 
