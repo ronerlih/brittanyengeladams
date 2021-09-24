3ef69faf18a2c5751b5be2a2ea4f354735138980
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2021-09-24 12:35:38 -0400

    redact two tessellations

--- website-data.js
+++ website-data.js
@@ -57,22 +57,22 @@ let WEBSITE_DATA = [
 	
 
 	
-		{		
-		name: "Emergent Landscape - Tessellation 1.(2020):",
-		img: "images/who_wood_picture1.png", 
-		blurb: "online tessellation, click to move.",
-		link: "https://www.brittanyengeladams.com/who-wood.html",
-		credit: "photo copyright: Photo Courtesy of artist, Tessellation by Brittany Engel-Adams"
-	},
+// 		{		
+// 		name: "Emergent Landscape - Tessellation 1.(2020):",
+// 		img: "images/who_wood_picture1.png", 
+// 		blurb: "online tessellation, click to move.",
+// 		link: "https://www.brittanyengeladams.com/who-wood.html",
+// 		credit: "photo copyright: Photo Courtesy of artist, Tessellation by Brittany Engel-Adams"
+// 	},
 	
 	
-	{
-		name: "Emergent Lanscape - Tessellation 2.(2020):",
-		img: "images/who-blue_picture1.png",
-		blurb: "online tessellation, click to move.",
-		link: "https://www.brittanyengeladams.com/who-blue.html",
-		credit: "photo copyright: Photo Courtesy of artist, Tessellation by Brittany Engel-Adams",
-	},
+// 	{
+// 		name: "Emergent Lanscape - Tessellation 2.(2020):",
+// 		img: "images/who-blue_picture1.png",
+// 		blurb: "online tessellation, click to move.",
+// 		link: "https://www.brittanyengeladams.com/who-blue.html",
+// 		credit: "photo copyright: Photo Courtesy of artist, Tessellation by Brittany Engel-Adams",
+// 	},
 	
 		{
 		name: "'Emergent Landscape' Tessellation 3",
