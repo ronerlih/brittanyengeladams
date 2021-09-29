fc0dc71c71c83430a95ad2987ff73a77e97dc367
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2021-09-28 20:07:42 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -3,7 +3,7 @@ let WEBSITE_DATA = [
 	{
 		name: "Publication oF Architecture of Memory (2021):",
 		img: "images/631C6A1A-CAAE-4458-9B91-2A5EEA266C44.JPG",
-		blurb: "Architecture of Memory' is radical spatializing practice and research  combining archival research, dance and technology modalities to bridge historical and contemporary conceptions of time and to draw haunting intersections, that might challenge our conception of humanity and shake the humanist foundation of modernist civilization. A Column from the publication of 'Architecture of Memory' is in the Movement Research Perofrmance Journal Issue 55 'no before no after'.",
+		blurb: "Architecture of Memory' is radical spatializing practice and research, combining archival research, dance and technology modalities to bridge historical and contemporary conceptions of time and to draw haunting intersections, that might challenge our conception of humanity and shake the humanist foundation of modernist civilization. A Column from the publication of 'Architecture of Memory' is in the Movement Research Perofrmance Journal Issue 55 'no before no after'.",
 		link: "https://movementresearch.org/publications/performance-journal/issue-55",
 		credit: "'photo courtesy of MRPJ.",
 	},
@@ -17,7 +17,7 @@ let WEBSITE_DATA = [
 	{
 		name: "Architecture of Memory (2021):",
 		img: "images/brit_floor_fabric_wow.png",
-		blurb: "'Architecture of Memory' (AOM) conceived in the wake of global pandemic, is a research project that weaves together personal and historical narrative, digital technology and proprioceptive research, to offer counter-glimpse at the materiality of the enigmatic, dark matter and componenets that both undergird and haunt humanity.",
+		blurb: "'Architecture of Memory' (AOM) conceived in the wake of global pandemic, is a research project that weaves together personal and historical narrative, digital technology and proprioceptive research, to offer counter-glimpse at the materiality of the enigmatic, dark matter and components that both undergird and haunt humanity.",
 		link: "https://movementresearch.org/publications/performance-journal/issue-55",
 		credit: "'disseminate the mesh' by Brittany Engel-Adams.",
 	},
