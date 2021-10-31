243bc55b4be56206aaca94dde2b55afba4acdbae
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2021-10-31 01:31:03 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -33,7 +33,7 @@ let WEBSITE_DATA = [
 	{
 		name: "Memory Dance (Avatar) (2020):",
 		img: "images/skeletalvideoedit.00_01_43_19.Still085.jpg", 
-		blurb: "'Memory Dance' (RAM) Explores constructs of identity using data-science and dance techniques that maps movement to the body.",
+		blurb: "'Memory Dance' (RAM) Uses digital-art and dance techniques to explore constructs of identity as a mathematical topology that maps to the body.",
 		link: "https://vimeo.com/436986855",
 		credit:"3d scan by Brittany Engel-Adams",
 	},
