c3a0c0c3ad6052e7f5b3009aa19efc2c53c8b51d
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-05-12 16:42:47 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -75,7 +75,7 @@ let WEBSITE_DATA = [
           {
 		name: "YouTube Improv",
 		img: "images/youtube22.jpeg",
-		blurb: "YouTube Improv is an immersive performance where YouTube video’s are randomly queued that are trending in the venue's proximity.",
+		blurb: "YouTube Improv is an immersive performance where YouTube video’s are randomly queued that are trending in the venue's proximity. Presented by Rutgers University at the Victoria J. Mastrobuono Theater.",
 		link: "https://www.artrabbit.com/events/youtube-improv",
 		credit: "'",
 	},
