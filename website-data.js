29286e1390ae2c537060b394261a8a6b7f16b17a
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-05-16 20:41:41 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -18,7 +18,7 @@ let WEBSITE_DATA = [
 		{
 		name: "HumanRobot",
 		img: "images/giphy2.gif",
-		blurb: "Human Robot is a performative work that places a performer and robot head to head, using generative adversarial neural networks and accumulative machine learning algorithms. Through a set of instructions that direct their actions - corresponding to the dimensions of the performance venue, and sensitive enough to respond to their environment and to each other. ",
+		blurb: "Human Robot is a live performance work between a dancer and robot, where they're placed head to head in battle, using generative adversarial neural networks and accumulative machine learning algorithms, as a movement score that correspond to the dimensions of the performance venue,  and are sensitive enough to respond to their environment and each other.",
 		link: "",
 		credit: "photo copyright: Brittany Engel-Adams, Ron Erlih",
 	},
