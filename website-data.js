9801554026a83b44d8ef04479344dfff42c404d5
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-05-19 18:00:08 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -153,7 +153,7 @@ let WEBSITE_DATA = [
 			{
 		name: "Architecture of Memory",
 		img: "images/mombrt.png",
-		blurb: "Architecture of Memory is a digital catalog with maps, tessellations, photographs, and 3d scans, inspired by the Saidiya Hartman's literary and archival methodology, the work combines archival research of ancestral history with critical fabulation, using AI machine learning algorithms, and kinect motion-capture technology. Publication of 'Architecture of Memory' in The Movement Research Performance Journal No 55.",
+		blurb: "Architecture of Memory is a digital catalog with maps, tessellations, photographs, and 3d scans, inspired by  Saidiya Hartman's literary and archival methodology 'critical fabulation', the work combines archival research of ancestral history with critical fabulation - using artificially intelligent machine learning algorithms, and kinect motion-capture technology. Publication of 'Architecture of Memory' in The Movement Research Performance Journal No 55.",
 		link: "https://movementresearch.org/publications/performance-journal/issue-55",
 		credit: "Brittany Engel-Adams",
 	},
