191865d2bcc2a43f75c3991b4f3a8d47348725ef
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-05-19 21:27:15 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -22,7 +22,7 @@ let WEBSITE_DATA = [
 		{
 		name: "Farewell 2020",
 		img: "images/3dscene.gif",
-		blurb: "3d scene, three.js, java. Is it possible to build artificial consciousness similar to human consciousness in digitized structures? If artificial intelligence will digitally reproduce the artificial structures of neurons, and the entire central nervous systems of humans, then an artificial nervous system counterpoint of cybernetic consciousness could be equivalent to human consciousness, that is capable of collecting and processing data, in a much larger database capacity than what’s possible at present.",
+		blurb: "Is it possible to build artificial consciousness similar to human consciousness in digitized structures? If artificial intelligence will digitally reproduce the artificial structures of neurons, and the entire central nervous systems of humans, then an artificial nervous system counterpoint of cybernetic consciousness could be equivalent to human consciousness, that is capable of collecting and processing data, in a much larger database capacity than what’s possible at present.",
 		link: "https://ronerlih.github.io/2020/",
 		credit: "Brittany Engel-Adams, Ron Erlih",
 	},	
