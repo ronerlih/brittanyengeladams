5b79159a9e3057ca8169cd3840908cba5db175ff
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-05-23 10:50:34 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -26,7 +26,7 @@ let WEBSITE_DATA = [
 		img: "images/maquettecc.gif",
 		blurb: "Inspired by the work of Walter Benjamin's book “The Work of Art in the Age of Mechanical Reproduction”, he uses the concept of the ‘aura’, to discuss how we use the machine in order to make art, and that we lose the ‘aura’ of the artwork, in his essay is in a sense a requiem to handmade art. The choreography of the Maquette echoes this chasm between the ‘aura’ of the handmade artwork, and the uniqueness that is deprived through the mechanical reproduction of it over time.",
 		link: "",
-		credit: "Brittany Engel-Adams, Ron Erlih in residency at Kugel Gips.",
+		credit: ".",
 	},
 	
 	
@@ -34,7 +34,7 @@ let WEBSITE_DATA = [
 	
 		{
 		name: "BodyMouth",
-		img: "images/bodymouth.png",
+		img: "images/bodymouthpic.png",
 		blurb: "'BodyMouth' uses pink trombone (speech synthesis) and turns the body into an instrument for speech. Harvestworks TEAM (Technology, Engineering, Art and Music) Lab Residency in New York.",
 		link: "",
 		credit: "",
