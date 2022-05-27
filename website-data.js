370fd3a16e236eabff7700e6d17c73a82d17b971
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-05-27 00:54:53 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -5,7 +5,7 @@ let WEBSITE_DATA = [
 		img: "images/human_robot_3.gif",
 		blurb: "'Human Robot' is a duet between a human and robot, inspired by choreographer Deborah Hay’s 1966 work ‘solo’ that is not really a solo, but rather a work for 16 dancers, and 8 tele-guided platforms and their operators, each dancer follows a solitary path that only episodically cross. Their ecology of movement reaches its climax when it is the platforms that move them across the stage, dignified as Apollos or stiff as planks.'",
 		link: "",
-		credit: "Choreographery + Engineering + Design: Ron Erlih and Brittany Engel-Adams.",
+		credit: "Choreographery + Engineer + Design: Ron Erlih and Brittany Engel-Adams.",
 	},
 	
 		{
@@ -13,9 +13,17 @@ let WEBSITE_DATA = [
 		img: "images/many.JPG",
 		blurb: "Inspired by the work of Walter Benjamin's book “The Work of Art in the Age of Mechanical Reproduction”, he uses the concept of the ‘aura’, to discuss how we use the machine in order to make art, and that we lose the ‘aura’ of the artwork, in his essay is in a sense a requiem to handmade art. The choreography of the Maquette echoes this chasm between the ‘aura’ of the handmade artwork, and the uniqueness that is deprived through the mechanical reproduction of it over time.",
 		link: "",
-		credit: "Choreographery + Engineering + Design: Ron Erlih and Brittany Engel-Adams.",
+		credit: "Choreographery + Engineer + Design: Ron Erlih and Brittany Engel-Adams.",
 	},
 	
+			{
+				
+		name: "Archival Research",
+		img: "images/woodcut.jpg",
+		blurb: "This remarkable ‘Woodcut’ aka ‘Mizrach’ was crafted by my great-great grandfather, Israel Manaschewitz. The original Mizrach is in Israel and belongs to my relatives. Manashewitz made one for each of his grandchildren when they were born. This Mizrach  was made for my grandmother Judy Engel, aka ‘Bubie’ (in Yiddish). Her grandparents, Israel Tzvi Manaschewitz and Chaya Miriam Manaschewitz originally from Brod Galicia, who later immigrated to the U.S in the 1880s. Chaya Manaschewitz, her maiden name was Landau, from the lineage of one of the most renowned scholars of his day, Rabbi Ezekiel Landau known by his book as the Noda be-Yehuda, and he is believed to be a descendant of the famous commentator of the 12th century, Rashi who lived most of his life in Wormiza, Germany.",
+		link: "https://movementresearch.org/publications/performance-journal/issue-55",
+		credit: ".",
+	},
 	
 			{
 				
