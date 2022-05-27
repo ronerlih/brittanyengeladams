48453b15e24d2a29a667483b96d34953ee4fc295
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-05-27 01:20:16 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -166,22 +166,7 @@ let WEBSITE_DATA = [
 	},
 	
 
-		
-	{
-		name: "Youtube Improv",
-		img: "images/YOUTUBEIMPROVPRESS.JPG",
-		blurb: "'YouTube Improv' There are two versions of 'Youtube Improv', the first version is the visual one, where projected on stage is the youtube webpage. The dancers interact with the youtube content and each other. The rules of the game confront the dancers and demand real time improvisations and different reactions to the video content. The second version is only the audio from the youtube webpage. The dancers are being prompted by only listening to the youtube clips that are randomly queued.  OPENING on Friday, 27 July at 6 pm. LOCATION: Victoria J. Mastrobuono Theater, Mason Gross Performing Arts Center, 85 George St, New Brunswick, New Jersey.",
-		link: "https://www.artconnect.com/mina/events/youtube-improv",
-		credit: ""
-	},
-	
-	  {
-		name: "YouTube Improv",
-		img: "images/YouTubeImprov2.gif",
-		blurb: "YouTube Improv is a performative work where a grid of youtube videos which are trending in the venue's proximity play while dancers on stage adjust their movement score according to the algorithmic orchestrated playlist. Presented by Rutgers University at the Victoria J. Mastrobuono Theater.",
-		link: "",
-		credit: "Choreography and Design: Ron Erlih, Brittany Engel-Adams.",
-	},
+
 	
 	
 	{
@@ -298,6 +283,25 @@ let WEBSITE_DATA = [
 		credit: "photo by Robert Maxwell",
 	},
 
+
+	
+	
+		
+	{
+		name: "Youtube Improv",
+		img: "images/YOUTUBEIMPROVPRESS.JPG",
+		blurb: "'YouTube Improv' There are two versions of 'Youtube Improv', the first version is the visual one, where projected on stage is the youtube webpage. The dancers interact with the youtube content and each other. The rules of the game confront the dancers and demand real time improvisations and different reactions to the video content. The second version is only the audio from the youtube webpage. The dancers are being prompted by only listening to the youtube clips that are randomly queued.  OPENING on Friday, 27 July at 6 pm. LOCATION: Victoria J. Mastrobuono Theater, Mason Gross Performing Arts Center, 85 George St, New Brunswick, New Jersey.",
+		link: "https://www.artconnect.com/mina/events/youtube-improv",
+		credit: ""
+	},
+	
+	  {
+		name: "YouTube Improv",
+		img: "images/YouTubeImprov2.gif",
+		blurb: "YouTube Improv is a performative work where a grid of youtube videos which are trending in the venue's proximity play while dancers on stage adjust their movement score according to the algorithmic orchestrated playlist. Presented by Rutgers University at the Victoria J. Mastrobuono Theater.",
+		link: "",
+		credit: "Choreography and Design: Ron Erlih, Brittany Engel-Adams.",
+	},
 	
 	
 // 		{
