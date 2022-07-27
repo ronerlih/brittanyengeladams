e27fa1d3a4a5b7382ab0b1af3b1b7b0b6798524b
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-07-27 18:45:06 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -208,7 +208,7 @@ let WEBSITE_DATA = [
 	
 	  {
 		name: "YouTube Improv",
-		img: "images/YouTubeImprov2.gif",
+		img: "images/YOUTUBEIMPROVPRESS.JPG",
 		blurb: "YouTube Improv is a performative work where a grid of youtube videos which are trending in the venue's proximity play while dancers on stage adjust their movement score according to the algorithmic orchestrated playlist. Presented by Rutgers University in 2018 at the Victoria J. Mastrobuono Theater.",
 		link: "",
 		credit: "",
