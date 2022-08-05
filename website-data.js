6a3ed8b1c7d07d1ded032a4c6896d7f02c55674e
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-05 10:33:05 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -188,7 +188,7 @@ let WEBSITE_DATA = [
 			{
 		name: "Emergent Landscape 2",
 		img: "images/wholives.png",
-		blurb: "Tessellation, magazine clippings, collage.",
+		blurb: "Tessellation, Digital-Collage.",
 		link: "https://www.brittanyengeladams.com/who-blue.html",
 		credit: "Brittany Engel-Adams.",
 	},
@@ -198,7 +198,7 @@ let WEBSITE_DATA = [
 	{
 		name: "Emergent Landscape 1",
 		img: "images/red-who_picture1.png",
-		blurb: "Tessellation, magazine clippings, collage.",
+		blurb: "Tessellation, Digital-Collage.",
 		link: "https://www.brittanyengeladams.com/red-who.html",
 		credit: "Brittany Engel-Adams",
 	},
