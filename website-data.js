b076ac2c20cb1d0108ac374df61f3cdd6b8189f6
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-05 08:41:32 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -228,13 +228,13 @@ let WEBSITE_DATA = [
 		credit: "Brittany Engel-Adams.",
 	},
 	
-// 		{
-// 		name: "Youtube Improv",
-// 		img: "images/YOUTUBEIMPROVPRESS.JPG",
-// 		blurb: "'YouTube Improv' There are two versions of 'Youtube Improv', the first version is the visual one, where projected on stage is the youtube webpage. The dancers interact with the youtube content and each other. The rules of the game confront the dancers and demand real time improvisations and different reactions to the video content. The second version is only the audio from the youtube webpage. The dancers are being prompted by only listening to the youtube clips that are randomly queued.  OPENING on Friday, 27 July at 6 pm. LOCATION: Victoria J. Mastrobuono Theater, Mason Gross Performing Arts Center, 85 George St, New Brunswick, New Jersey.",
-// 		link: "",
-// 		credit: ""
-// 	},
+		{
+		name: "Youtube Improv",
+		img: "images/YOUTUBEIMPROVPRESS.JPG",
+		blurb: "'YouTube Improv' There are two versions of 'Youtube Improv', the first version is the visual one, where projected on stage is the youtube webpage. The dancers interact with the youtube content and each other. The rules of the game confront the dancers and demand real time improvisations and different reactions to the video content. The second version is only the audio from the youtube webpage. The dancers are being prompted by only listening to the youtube clips that are randomly queued.  OPENING on Friday, 27 July at 6 pm. LOCATION: Victoria J. Mastrobuono Theater, Mason Gross Performing Arts Center, 85 George St, New Brunswick, New Jersey.",
+		link: "",
+		credit: ""
+	},
 	
 	
 	
@@ -266,7 +266,7 @@ let WEBSITE_DATA = [
 	
 		{
 		name: "Human Robot",
-		img: "images/Ahumanrobotstill",
+		img: "images/Ahumanrobotstill.png",
 		blurb: "Inspired Walter Benjamin's “The Work of Art in the Age of Mechanical Reproduction”, and his concept of the ‘aura’, to describe using the machine in order to make art is in a sense a requiem to handmade art.'",
 		link: "",
 		credit: "",
