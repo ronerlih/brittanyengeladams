f182470074ec6f28387585b0b64197eaa8999cc9
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-06 09:12:08 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -328,7 +328,7 @@ let WEBSITE_DATA = [
 		{
 		name: "Human Robot",
 		img: "images/Humanrobotron.jpg",
-		blurb: "'Human Robot' is a duet between a human and Robot, uses adversarial neural networks and accumulative machine learning algorithms, inspired by choreographer Deborah Hay’s 1966 work ‘solo’ that is not really a solo, but rather a work where a dancer and 8 tele-guided platforms and their operators, each follows a solitary path that only episodically crosses. Their ecology of movement reaches its climax when it is the platforms that move them across the stage, dignified as Apollos or stiff as planks.'",
+		blurb: "Human Robot uses adversarial neural networks and accumulative machine learning algorithms, and is inspired by the work of Walter Benjamin's book “The Work of Art in the Age of Mechanical Reproduction”, he uses the concept of the ‘aura’, to discuss how we use the machine in order to make art, and that we lose the ‘aura’ of the artwork, in his essay is in a sense a requiem to handmade art.'",
 		link: "",
 		credit: "",
 	},
