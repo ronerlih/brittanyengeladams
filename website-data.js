8b352014d016e7dd646e8a93239b098a62fc607a
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-06 04:02:53 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -24,6 +24,13 @@ let WEBSITE_DATA = [
 	
 	
 	
+			{
+		name: "Architecture of Memory (Publication)",
+		img: "images/mrpj.jpg", 
+		blurb: "’Architecture of Memory' brings together personal narrative and historical accounts using motion capture and machine learning technology as a way to connect past, present and future. Publication, 2021 Movement Research Performance Journal, Issue #55.",
+		link: "https://movementresearch.org/publications/performance-journal/issue-55",
+		credit:"",
+	},
 	
 	
 	
@@ -92,13 +99,6 @@ let WEBSITE_DATA = [
 	
 
 	
-			{
-		name: "Architecture of Memory (Publication)",
-		img: "images/mrpj.jpg", 
-		blurb: "’Architecture of Memory' brings together personal narrative and historical accounts using motion capture and machine learning technology as a way to connect past, present and future. Publication, 2021 Movement Research Performance Journal, Issue #55.",
-		link: "https://movementresearch.org/publications/performance-journal/issue-55",
-		credit:"",
-	},
 	
 	
 	
