904ae3d0a24b734bece4f02c08d646a44d7a981f
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-08 07:01:23 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -265,6 +265,7 @@ let WEBSITE_DATA = [
 	},
 	
 	
+
 	
 	{
 		name: "Time Loop",
@@ -278,16 +279,6 @@ let WEBSITE_DATA = [
 	
 	
 	
-		
-		{
-		name: "Youtube Improv (Webpage)",
-		img: "images/YouTubeimprovhigh.gif",
-		blurb: "YouTube Improv webpage is an automated webpage that collects ‘trending’ videos from Youtube and plays them randomly.",
-		link: "https://vimeo.com/manage/videos/429453098",
-		credit: ""
-	},
-	
-	
 	
 	
 		{
@@ -297,6 +288,17 @@ let WEBSITE_DATA = [
 		link: "https://www.guggenheim.org/initiatives/works-process/virtual-commissions",
 		credit: "",
 	},
+	
+	
+		{
+		name: "Youtube Improv (Webpage)",
+		img: "images/YouTubeimprovhigh.gif",
+		blurb: "YouTube Improv webpage is an automated webpage that collects ‘trending’ videos from Youtube and plays them randomly.",
+		link: "https://vimeo.com/manage/videos/429453098",
+		credit: ""
+	},
+	
+	
 	
 	{
 		name: "Transmedia Storytelling in the Age of Proximity",
