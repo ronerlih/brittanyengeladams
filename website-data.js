0447432d4b8271a889ddff51eb8e035be550f2aa
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-09 21:50:11 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -325,7 +325,7 @@ let WEBSITE_DATA = [
 		
 		{
 		name: "Cape Cod Modern Architecture (Artist Residency)",
-		img: "images/CAPECODMHTPRESSIMAGE.JPG",
+		img: "images/capebook.png",
 		blurb: "Cape Cod Modern House Trust Artist Residency 2020-2021. Where I presented two pieces that I worked on while in Wellfleet, 'Architecture of Memory' and Hand Dance.", 
 		link: "https://provincetownindependent.org/wellfleets-modern-houses-are-still-laboratories-for-collaboration/",
 		credit: "",
