5742b3801d4880dc73472da3b8c62088bf1e80aa
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-10 00:51:18 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -5,7 +5,7 @@ let WEBSITE_DATA = [
 			{
 		name: "BodyMouth",
 		img: "images/bodymouth202.jpeg",
-		blurb: "'BodyMouth' turns the body into an instrument for speech, made in collaboration between Kat Mustatea and Sidney San Martín. Fourthcoming Symposium and Exhibition; The next New Media Caucus Symposium takes place from September 30th-October 2nd 2022 at Virginia Tech and will focus on the theme of 'Future Bodies'."",
+		blurb: "'BodyMouth' turns the body into an instrument for speech, made in collaboration between Kat Mustatea and Sidney San Martín. Fourthcoming Symposium and Exhibition; The next New Media Caucus Symposium takes place from September 30th-October 2nd 2022 at Virginia Tech and will focus on the theme of 'Future Bodies'.",
 		link: "https://futurebodies.newmediacaucus.org/",
 		credit: "",
 	},
