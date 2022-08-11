2aadb922a95948704b07417c0dcb61eb069aff76
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-12 01:25:00 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -32,7 +32,7 @@ let WEBSITE_DATA = [
 	
 	{
 		name: "Human Robot",
-		img: "images/NODESP.jpg",
+		img: "images/human_robot_3.gif",
 		blurb: "Human Robot uses adversarial neural networks and machine learning algorithms, inspired by the work of Walter Benjamin's book “The Work of Art in the Age of Mechanical Reproduction”, he uses the concept of the ‘aura’, to discuss how we use the machine in order to make art, and that we lose the ‘aura’ of the artwork, in his essay is in a sense a requiem to handmade art.'",
 		link: "",
 		credit: "",
