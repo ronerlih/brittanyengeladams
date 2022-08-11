935e2b3a4a4a68d1f1e8cc23e7b573ae28b8a0ac
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-11 05:59:28 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -355,14 +355,17 @@ let WEBSITE_DATA = [
 	},
 	
 	
-	
-					{		
-		name: "'Wood'",
-		img: "images/whowood.gif", 
-		blurb: "Tessellation, Digital-Collage.",
-		link: "https://www.brittanyengeladams.com/who-wood.html",
+		{
+				
+		name: "Family Archive",
+		img: "images/mizrach.jpg",
+		blurb: "This ‘Mizrach’ was crafted by my great-great grandfather, Israel Manaschewitz. This Mizrach  was made for my grandmother. Her grandparents, Israel Tzvi Manaschewitz and Chaya Miriam Manaschewitz originally from Brod Galicia, who later immigrated to the U.S in the 1880s. Chaya Manaschewitz, her maiden name was Landau, from the lineage of one of the most renowned scholars of his day, Rabbi Ezekiel Landau known by his book as the Noda be-Yehuda, and he is believed to be a descendant of the famous commentator of the 12th century, Rashi.",
+		link: "https://movementresearch.org/publications/performance-journal/issue-55",
 		credit: "",
 	},
+	
+	
+	
 		
 	
 	
@@ -392,18 +395,17 @@ let WEBSITE_DATA = [
 	
 	
 	
-		{
-				
-		name: "Family Archive",
-		img: "images/mizrach.jpg",
-		blurb: "This ‘Mizrach’ was crafted by my great-great grandfather, Israel Manaschewitz. This Mizrach  was made for my grandmother. Her grandparents, Israel Tzvi Manaschewitz and Chaya Miriam Manaschewitz originally from Brod Galicia, who later immigrated to the U.S in the 1880s. Chaya Manaschewitz, her maiden name was Landau, from the lineage of one of the most renowned scholars of his day, Rabbi Ezekiel Landau known by his book as the Noda be-Yehuda, and he is believed to be a descendant of the famous commentator of the 12th century, Rashi.",
-		link: "https://movementresearch.org/publications/performance-journal/issue-55",
+	
+	
+	
+				{		
+		name: "'Wood'",
+		img: "images/whowood.gif", 
+		blurb: "Tessellation, Digital-Collage.",
+		link: "https://www.brittanyengeladams.com/who-wood.html",
 		credit: "",
 	},
-	
-	
-	
-	
+		
 	
 
 	
