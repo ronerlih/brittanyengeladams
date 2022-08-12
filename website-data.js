d3343825d4a51af149407a64c2e4ec752464fefb
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-12 03:25:06 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -32,10 +32,12 @@ let WEBSITE_DATA = [
 		{
 		name: "Human-Robot",
 		img: "images/human_robot_3.gif",
-		blurb: " ‘Human-Robot' is a duet between a human and Robot,  inspired by choreographer Deborah Hay’s 1966 work ‘solo’ that is not really a solo, but rather a work where a dancer and 8 tele-guided platforms and their operators, each follows a solitary path that only episodically crosses. Their ecology of movement reaches its climax when it is the platforms that move them across the stage, dignified as Apollos or stiff as planks.'",
+		blurb: " Human-Robot: adversarial neural networks and machine learning algorithms. 'Human-Robot' is a duet between a human and Robot; inspired by choreographer Deborah Hay’s 1966 work ‘solo’ that is not really a solo, but rather a work where a dancer and 8 tele-guided platforms and their operators, each follows a solitary path that only episodically crosses. Their ecology of movement reaches its climax when it is the platforms that move them across the stage, dignified as Apollos or stiff as planks.'",
 		link: "",
 		credit: "",
 	},
+	
+	
 	
 				{
 		name: "Emergent Landscape 2",
@@ -49,8 +51,6 @@ let WEBSITE_DATA = [
 	
 	
 	
-	
-		
 	
 			{
 		name: "Joffrey NYC Jazz & Contemporary Summer Intensive Faculty",
@@ -179,7 +179,7 @@ let WEBSITE_DATA = [
 		{
 		name: "Maquette Research 1",
 		img: "images/Maquettecolor.jpg",
-		blurb: "Maquette 2021: omni-wheels, arduino, c++, bluetooth, acrylic, breadboard, and batteries. Guided by a set of instructions that direct the actions of the maquette and its operator in space together.",
+		blurb: "omni-wheels, arduino, c++, bluetooth, acrylic, breadboard, and batteries. Guided by a set of instructions that direct the actions of the maquette and its operator in space together.",
 		link: "",
 		credit: " .",
 	},
