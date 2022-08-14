b52a2cc31d64b60b511c2247de98b9c8d81aac5b
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-14 23:28:59 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -428,7 +428,7 @@ let WEBSITE_DATA = [
 				
 		name: "Archival Method",
 		img: "images/mizrach.jpg",
-		blurb: "Remnants of Ancestors: A Mizrach crafted by Israel Manaschewitz (great-great grandfather), for my grandmother, Judy Engel from the lineage of one of the most renowned scholars of his day, Rabbi Ezekiel Landau known by his book as the Noda be-Yehuda, and he is believed to be a descendant of the famous commentator of the 12th century, Rashi.",
+		blurb: "Family Gems: A Mizrach crafted by Israel Manaschewitz (great-great grandfather), for my grandmother, Judy Engel from the lineage of one of the most renowned scholars of his day, Rabbi Ezekiel Landau known by his book as the Noda be-Yehuda, and he is believed to be a descendant of the famous commentator of the 12th century, Rashi.",
 		link: "https://movementresearch.org/publications/performance-journal/issue-55",
 		credit: "",
 	},
