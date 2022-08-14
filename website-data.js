1fb57107b88cc285a937be4532d4cd92fe36c233
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-14 19:49:07 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -67,7 +67,7 @@ let WEBSITE_DATA = [
 	
 		{
 		name: "Maquette",
-		img: "images/maquettebox202.jpg",
+		img: "images/maquettereebok.png",
 		blurb: "omni-wheels, arduino, c++, bluetooth, acrylic, breadboard, and batteries. Guided by a set of instructions that direct the actions of the maquette and its operator in space together. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork and the uniqueness that is deprived through the mechanical reproduction of it.",
 		link: "",
 		credit: " .",
