5799e37c25bb896ea5840ed9641472dfadfd9fe4
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-14 23:15:33 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -270,7 +270,7 @@ let WEBSITE_DATA = [
 	
 {
 		name: "Emergent Landscape 1",
-		img: "images/redscroll202.gif",
+		img: "images/redzoom.png",
 		blurb: "Tessellation, Digital-Collage.",
 		link: "https://www.brittanyengeladams.com/red-who.html",
 		credit: "",
