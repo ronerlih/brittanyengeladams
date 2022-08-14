83852e7f761c4f6088d4bbee6b9d5b90123fec14
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-14 22:12:01 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -416,7 +416,7 @@ let WEBSITE_DATA = [
 			{
 				
 		name: "Avatar",
-		img: "images/biz.jpeg",
+		img: "images/metal.png",
 		blurb: "Is it possible to build artificial consciousness similar to human consciousness in digitized structures? If artificial intelligence will digitally reproduce the artificial structures of neurons, and the entire central nervous systems of humans, then an artificial nervous system counterpoint of cybernetic consciousness could be equivalent to human consciousness, that is capable of collecting and processing data, in a much larger database capacity than what’s possible at present.",
 		link: "",
 		credit: "",
