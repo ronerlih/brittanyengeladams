c31ef3aa183c6ae2f1844939716af16fc88c1be9
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-14 21:35:07 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -486,7 +486,7 @@ let WEBSITE_DATA = [
 	
 	
 		{
-		name: "Hand Dance (Research)",
+		name: "Hand Dance Development",
 		img: "images/HanddanceTest.jpg",
 		blurb: "Techniques used to make the hand-dance film: Example 1: The first option, a ‘disintegrating figure’, uses choreographic and digital techniques to erase parts of the body from the footage. (according to light intensity/ contours / color averages). The figure moves in-and-out of the frame (repeated entrances and exits). The body is obscured and is fading as if disintegrating into the background of the scene. (a blending quality as if a statue in ruins) For example, only the sternum and hands would be made visible. This google image, has the general resemblance to the quality of blending, tweaking, and manipulating the visual in different ways.",
 		link: "",
