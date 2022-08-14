0a7cd7b655e47786a0ffef805d17025b1fed3469
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-14 21:31:53 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -238,7 +238,7 @@ let WEBSITE_DATA = [
 	
 			{
 		name: "Time Loop (Performance)",
-		img: "images/timeloops.jpg",
+		img: "images/Loops2018high.gif",
 		blurb: "'Time Loops uses live camera feed to record, loop, and project the movement's of the performers onto the stage, at Kumble Theater in Brooklyn NY.",
 		link: "",
 		credit: "",
