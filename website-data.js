6c327debf672678769a2cd625393313076239a6a
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-14 19:51:43 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -48,7 +48,7 @@ let WEBSITE_DATA = [
 	
 	{
 		name: "Transmedia Storytelling in the Age of Proximity",
-		img: "images/Intersection123.jpg",
+		img: "images/intersecttech.png",
 		blurb: "Introducing LTED (a new proximity technology from Qualcomm). Proximity is changing consumer experiences. With mobile applications and proximity technologies like bluetooth beacons, and LTE Direct (LTED), synchronized campaigns will harmonize the media noise and offer deeper engagement, re-engage in a public space, respond to a call to action on their phones, and connect the dots with attribution in a retail store. The emergence of LTED and proximity technologies will also bring the context and intelligence of digital advertising to the real world. And it’s going to change the mobile, retail, and out-of-home industries forever.",
 		link: "https://www.intersection.com/",
 		credit: "",
