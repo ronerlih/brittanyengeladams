9c851f515d197a117c54221c5a3014126ca8ce51
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-16 21:04:21 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -21,7 +21,7 @@ let WEBSITE_DATA = [
 		name: "Time Loop - Live",
 		img: "images/Loops2018high.gif",
 		blurb: "'Time Loops uses live camera feed to record, loop, and project the movement's of the performers onto the stage performed by Dancers from Cynthia King Dance School at Kumble Theater.",
-		link: "",
+		link: "https://cynthiakingdance.com/class-schedule?gclid=Cj0KCQjwgO2XBhCaARIsANrW2X1aUqj0j5C2PEWQPMPHAnZ2sUCnNaKwBJil8pthRA1qWpSFH-KO8kAaAoiyEALw_wcB",
 		credit: "",
 	},
 	
