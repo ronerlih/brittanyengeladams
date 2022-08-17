2e2c67579157f5c9262e9b738d98590fc9aab70d
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-17 03:31:19 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -240,7 +240,7 @@ let WEBSITE_DATA = [
 	{
 		name: "BodyMouth Research",
 		img: "images/harvestworksepoch.png",
-		blurb: "'BodyMouth in Residency at Harvestworks with Kat Mustatea and Sidney San Martín. Harvestworks' mission is to present experimental artworks created in collaboration with our Technology, Engineering, Art and Music (TEAM) Lab.",
+		blurb: "'BodyMouth in Residency at Harvestworks. Harvestworks' mission is to present experimental artworks created in collaboration with our Technology, Engineering, Art and Music (TEAM) Lab.",
 		link: "https://www.harvestworks.org/",
 		credit: "",
 	},
