766c2db266cd91e43938821a2219cd0e9a04fe54
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-18 18:18:40 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -15,13 +15,6 @@ let WEBSITE_DATA = [
 	
 	
 	
-					{
-		name: "The Bessies Canceled Work Archive",
-		img: "images/be$$ie_44_load_54.png",
-		blurb: "2020 ARCHIVE OF NYC CANCELED DANCE PERFORMANCES (Performances Canceled Due To COVID-19 Pandemic.)",
-		link: "http://bessies.org/canceled-work-2020/",
-		credit: "",
-	},
 	
 	
 		{
@@ -44,6 +37,13 @@ let WEBSITE_DATA = [
 	
 	
 	
+					{
+		name: "The Bessies Canceled Work Archive",
+		img: "images/be$$ie_44_load_54.png",
+		blurb: "2020 ARCHIVE OF NYC CANCELED DANCE PERFORMANCES (Performances Canceled Due To COVID-19 Pandemic.)",
+		link: "http://bessies.org/canceled-work-2020/",
+		credit: "",
+	},
 	
 	
 	
