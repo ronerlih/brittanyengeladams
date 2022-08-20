7ac8165d1ebca763f47c6eae1f33c2cac3655d4b
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-21 00:19:25 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -183,7 +183,7 @@ let WEBSITE_DATA = [
 	 {
 		name: "Performance of YouTube Improv",
 		img: "images/YouTubeImprov2.gif",
-		blurb: "YouTube Improv displays a grid of youtube videos play, which are trending in the venue's proximity, while performers on stage adjust their movement score according to the algorithmic orchestrated playlist, at the Victoria J. Mastrobuono Theater at Rutgers University, in New Brunswick NJ.",
+		blurb: "YOUTUBE IMPROV VERSION 2: Brittany Engel-Adams and Ron Erlih new performance ‘YouTube Improv’ projected on top of the Victoria J. Mastrobuono Theater will be playing a grid of random youtube videos which are trending in the venue's proximity. The dancers on stage will then have to adjust their improvised score according to the algorithmic orchestrated playlist, as we all do. OPENING on Friday, 27 July at 6 pm. LOCATION: Victoria J. Mastrobuono Theater, Mason Gross Performing Arts Center, 85 George St, New Brunswick, New Jersey.",
 		link: "",
 		credit: "",
 	},
