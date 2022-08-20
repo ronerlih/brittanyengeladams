40f4b7c873e581f0e106855d4b2495b7713443fe
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-20 19:13:07 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -57,20 +57,19 @@ let WEBSITE_DATA = [
 // 		credit: "",
 // 	},
 	
+
 	
 	
-	
-	
+		
 			{
-		name: "Maquette",
-		img: "images/grabbag_69.png",
-		blurb: "omni-wheels, arduino, c++, bluetooth, acrylic, breadboard, and batteries. Guided by a set of instructions that direct the actions of the maquette and its operator in space together. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork and the uniqueness that is deprived through the mechanical reproduction of it.",
-		link: "",
-		credit: " .",
+	
+				
+		name: "Family Archive",
+		img: "images/family1.jpg",
+		blurb: "Archival Research: A Mizrach crafted by Israel Manaschewitz (great-great grandfather), for my grandmother, Judy Engel from the lineage of one of the most renowned scholars of his day, Rabbi Ezekiel Landau known by his book as the Noda be-Yehuda, and he is believed to be a descendant of the famous commentator of the 12th century, Rashi.",
+		link: "https://movementresearch.org/publications/performance-journal/issue-55",
+		credit: "",
 	},
-	
-	
-	
 	
 
 	
@@ -95,18 +94,19 @@ let WEBSITE_DATA = [
 		credit:"",
 	},
 	
+
+	
+		
+	
+	
 	
 			{
-	
-				
-		name: "Family Archive",
-		img: "images/family1.jpg",
-		blurb: "Archival Research: A Mizrach crafted by Israel Manaschewitz (great-great grandfather), for my grandmother, Judy Engel from the lineage of one of the most renowned scholars of his day, Rabbi Ezekiel Landau known by his book as the Noda be-Yehuda, and he is believed to be a descendant of the famous commentator of the 12th century, Rashi.",
-		link: "https://movementresearch.org/publications/performance-journal/issue-55",
-		credit: "",
+		name: "Maquette",
+		img: "images/grabbag_69.png",
+		blurb: "omni-wheels, arduino, c++, bluetooth, acrylic, breadboard, and batteries. Guided by a set of instructions that direct the actions of the maquette and its operator in space together. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork and the uniqueness that is deprived through the mechanical reproduction of it.",
+		link: "",
+		credit: " .",
 	},
-	
-	
 	
 
 					{
@@ -138,6 +138,13 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 	
+		{
+		name: "Farewell 2020",
+		img: "images/3dscene.gif",
+		blurb: "Thanks:#Threejs, #react-three-fiber, #drei",
+		link: "",
+		credit: "",
+	},
 	
 			{
 		name: "Artist Residency - Cape Cod Modern Architecture",
@@ -269,13 +276,7 @@ let WEBSITE_DATA = [
 	},
 
 	
-			{
-		name: "Farewell 2020",
-		img: "images/3dscene.gif",
-		blurb: "Thanks:#Threejs, #react-three-fiber, #drei",
-		link: "",
-		credit: "",
-	},
+		
 
 
 	
