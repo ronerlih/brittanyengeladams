828ded99652f2ea8e5eea7135bec68e709dbeff0
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-22 01:34:27 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -118,7 +118,7 @@ let WEBSITE_DATA = [
 	
 			{
 		name: "Human-Robot (Maquette 2021)",
-		img: "images/circuit____2rain_44.png",
+		img: "images/circuit_55.png",
 		blurb: "omni-wheels, arduino, c++, bluetooth, acrylic, breadboard, and batteries. Guided by a set of instructions that direct the actions of the maquette and its operator in space together. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork and the uniqueness that is deprived through the mechanical reproduction of it.",
 		link: "",
 		credit: " .",
