7704a12afe9411f6936f23d58fca5a0ef79c2f6e
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-22 02:07:58 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -357,7 +357,7 @@ let WEBSITE_DATA = [
 			
 {
 		name: "Hand Dance (Motion Capture Research)",
-		img: "images/canned.gif",
+		img: "images/annlazar_ed.gif",
 		blurb: "Techniques used to make the hand-dance film: Example 2: The second option, a ‘displaced figure’ Choreography transposes hand gestures to other body parts (like the legs, or the torso). Programatically overlaying and transposing the body part and on top of the video footage or an alternative video.",
 		link: "",
 		credit: "",
