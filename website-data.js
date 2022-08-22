0e7f9c348eddaf7a230ce65ca0cab59da1c0afe8
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-22 16:49:09 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -51,7 +51,7 @@ let WEBSITE_DATA = [
 	
 		{
 		name: "LTED Proximity Technology from Qualcomm",
-		img: "images/intersecttech.png",
+		img: "images/Qualcomm.png",
 		blurb: "Proximity is changing consumer experiences. Mobile applications and proximity technologies like bluetooth beacons, and LTE Direct (LTED), synchronized campaigns infiltrate the media noise to offer a totalizing engagement; a coercive call to action, on their phones, or in a public space.",
 		link: "https://www.intersection.com/",
 		credit: "",
