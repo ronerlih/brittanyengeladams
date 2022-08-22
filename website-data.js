832925700b9a95428d2216d2a4ead2027b75f6d2
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-23 02:22:12 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -36,7 +36,7 @@ let WEBSITE_DATA = [
 	
 	{
 		name: "The Cape Cod Modern House Trust (Artist Residency)",
-		img: "images/capecodmoderntrust.png",
+		img: "images/capecodmoderntrust.jpg",
 		blurb: "The Cape Cod Modern House Trust (CCMHT) was founded to collect, archive, and share documentation of the Outer Cape’s exceptional modern architecture, restore a group of important, endangered modern houses, and relaunch those houses as platforms for new creative work.", 
 		link: "https://ccmht.org/residency",
 		credit: "",
