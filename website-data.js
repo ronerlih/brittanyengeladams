823817e7c1439bb78df65438df7c6264d02201f9
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-22 10:28:51 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -249,29 +249,13 @@ let WEBSITE_DATA = [
 	
 	
 	
-	
-	
-	
-	
-	
-	
-			
-	
-		
-	
-
-	
-
-	
-	
-	
-	
-	
-	
-
-	
-	
-	
+	{
+		name: "Youtube Improv--Website",
+		img: "images/YouTubeimprovhigh.gif",
+		blurb: "‘Youtube improv’ live Automated Website collects random videos and randomizing the outcome. Ron Erlih and I wrote a software that plays trending youtube videos in a random order as a score for improvisation. This software expands improvisation, it provides more content to draw inspiration from. The tool produces spontaneity and indeterminacy and also produces videos. Working with the data brings a quality of data to the work, while also bringing the authenticity of the real, through the videos which depict actual people, real events, media and pop culture.",
+		link: "https://vimeo.com/manage/videos/429453098",
+		credit: ""
+	},
 
 		
 	
@@ -297,14 +281,7 @@ let WEBSITE_DATA = [
 	
 	
 	
-	
-	{
-		name: "Youtube Improv--Website",
-		img: "images/YouTubeimprovhigh.gif",
-		blurb: "‘Youtube improv’ live Automated Website collects random videos and randomizing the outcome. Ron Erlih and I wrote a software that plays trending youtube videos in a random order as a score for improvisation. This software expands improvisation, it provides more content to draw inspiration from. The tool produces spontaneity and indeterminacy and also produces videos. Working with the data brings a quality of data to the work, while also bringing the authenticity of the real, through the videos which depict actual people, real events, media and pop culture.",
-		link: "https://vimeo.com/manage/videos/429453098",
-		credit: ""
-	},
+
 
 	
 		
