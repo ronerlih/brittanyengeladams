718d59caf5a76d57facee6be77f682bcfc3165d1
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-22 04:57:16 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -46,7 +46,7 @@ let WEBSITE_DATA = [
 	
 	{
 		name: "Rutgers University's Fall 2021 Student Dance Concert Curator",
-		img: "images/ode_Dnietz_psYerusalmury_swing_re.png", 
+		img: "images/masongross77.pdf", 
 		blurb: "Mason Gross School of the Arts' BFA Dance Students created work for The Fall 2021 Student Dance Concert as a response to a theatrical production, or dance performance, from the 19th century through the 20th century, reimagined for the 21st century stage.",
 		link: "https://www.masongross.rutgers.edu/degrees-programs/dance/",
 		credit: "",
