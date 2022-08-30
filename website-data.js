92e8318ff0312e79ddf51f9c4c48afd8eef03d15
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-30 15:43:18 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -29,6 +29,10 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 	
+	
+	
+	
+	
 		{
 		name: "BodyMouth/Ielele -- Artist Residency",
 		img: "images/8182811802118118281182023o18118229192023o181119.jpg",
@@ -37,7 +41,13 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 	
-	
+			{
+		name: "Hand Dance (Research)",
+		img: "images/11414121191918.gif",
+		blurb: "Techniques used to make the hand-dance film: Example 1: The first option, a ‘disintegrating figure’, uses choreographic and digital techniques to erase parts of the body from the footage. (according to light intensity/ contours / color averages). The figure moves in-and-out of the frame (repeated entrances and exits). The body is obscured and is fading as if disintegrating into the background of the scene. (a blending quality as if a statue in ruins) For example, only the sternum and hands would be made visible. This google image, has the general resemblance to the quality of blending, tweaking, and manipulating the visual in different ways.",
+		link: "https://stackoverflow.com/questions/25153941/opencv-erode-and-dilate-switched",
+		credit: "",
+	},
 	
 	
 	
@@ -302,13 +312,7 @@ let WEBSITE_DATA = [
 	
 	
 	
-			{
-		name: "Hand Dance (Research)",
-		img: "images/11414121191918.gif",
-		blurb: "Techniques used to make the hand-dance film: Example 1: The first option, a ‘disintegrating figure’, uses choreographic and digital techniques to erase parts of the body from the footage. (according to light intensity/ contours / color averages). The figure moves in-and-out of the frame (repeated entrances and exits). The body is obscured and is fading as if disintegrating into the background of the scene. (a blending quality as if a statue in ruins) For example, only the sternum and hands would be made visible. This google image, has the general resemblance to the quality of blending, tweaking, and manipulating the visual in different ways.",
-		link: "https://stackoverflow.com/questions/25153941/opencv-erode-and-dilate-switched",
-		credit: "",
-	},
+	
 	
 	
 	
