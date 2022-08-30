0b35d5d4b6b2ee9b33c849fd804c4f3530fc58d4
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-30 17:02:28 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -319,7 +319,7 @@ let WEBSITE_DATA = [
 							{
 				
 		name: "Human-Robot -- Research",
-		img: "images/12212018_113518931_162119_755_17215518119621311_12925182025_2.jpg",
+		img: "images/giphy1humanrobot.gif",
 		blurb: "Is it possible to build artificial consciousness similar to human consciousness in digitized structures? If artificial intelligence will digitally reproduce the artificial structures of neurons, and the entire central nervous systems of humans, then an artificial nervous system counterpoint of cybernetic consciousness could be equivalent to human consciousness, that is capable of collecting and processing data, in a much larger database capacity than what’s possible at present.",
 		link: "",
 		credit: "",
@@ -392,13 +392,13 @@ let WEBSITE_DATA = [
 	
 	
 	
-				{
-		name: "Human_Robot -- Research",
-		img: "images/giphy1humanrobot.gif", 
-		blurb: "'‘Human-Robot': adversarial neural networks, and machine learning algorithms; is a duet between a human and Robot,  inspired by choreographer Deborah Hay’s 1966 work ‘solo’ that is not really a solo, but rather a work where a dancer and 8 tele-guided platforms and their operators, each follows a solitary path that only episodically crosses. Their ecology of movement reaches its climax when it is the platforms that move them across the stage, dignified as Apollos or stiff as planks.",
-		link: "",
-		credit:"",
-	},
+// 				{
+// 		name: "Human_Robot -- Research",
+// 		img: "images/", 
+// 		blurb: "'‘Human-Robot': adversarial neural networks, and machine learning algorithms; is a duet between a human and Robot,  inspired by choreographer Deborah Hay’s 1966 work ‘solo’ that is not really a solo, but rather a work where a dancer and 8 tele-guided platforms and their operators, each follows a solitary path that only episodically crosses. Their ecology of movement reaches its climax when it is the platforms that move them across the stage, dignified as Apollos or stiff as planks.",
+// 		link: "",
+// 		credit:"",
+// 	},
 	
 	
 	
