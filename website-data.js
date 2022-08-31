783e6067e039d1303a51d0251a6ba5d799025fcd
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-08-30 22:11:20 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -28,13 +28,7 @@ let WEBSITE_DATA = [
 	
 	
 	
-			{
-		name: "Human-Robot",
-		img: "images/11842191415_218514201184.jpg",
-		blurb: "Guided by a set of instructions that direct the actions of the maquette and its operator in space together. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork and the uniqueness that is deprived through the mechanical reproduction of it.",
-		link: "",
-		credit: " .",
-	},
+	
 	
 	
 
@@ -338,6 +332,17 @@ let WEBSITE_DATA = [
 // 		credit: "",
 // 	},
 	
+	
+	
+	
+	
+			{
+		name: "Human-Robot",
+		img: "images/11842191415_218514201184.jpg",
+		blurb: "Guided by a set of instructions that direct the actions of the maquette and its operator in space together. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork and the uniqueness that is deprived through the mechanical reproduction of it.",
+		link: "",
+		credit: " .",
+	},
 	
 	
 		{
