afdd5c90d3574255bea7778b92b1bb3481462e02
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-09-01 16:57:09 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -492,13 +492,6 @@ let WEBSITE_DATA = [
 	
 	
 	
-				{
-		name: "Maquette 2021",
-		img: "images/1125695156993125699515951951.jpg",
-		blurb: "Inspired by choreographer Deborah Hay’s 1966 work ‘solo', that is not really a solo - but rather a work where a dancer and 8 tele-guided platforms and their operators, each follows a solitary path that only episodically crosses. Their ecology of movement reaches its climax when it is the platforms that move them across the stage, dignified as Apollos or stiff as planks.",
-		link: "",
-		credit: " .",
-	},
 	
 	
 	
@@ -530,6 +523,15 @@ let WEBSITE_DATA = [
 		credit:"Photograph of my maternal great-grandparents; Chana and David Rackman (Rachmilewich).",
 	},
 	
+	
+	
+				{
+		name: "Maquette 2021",
+		img: "images/1125695156993125699515951951.jpg",
+		blurb: "Inspired by choreographer Deborah Hay’s 1966 work ‘solo', that is not really a solo - but rather a work where a dancer and 8 tele-guided platforms and their operators, each follows a solitary path that only episodically crosses. Their ecology of movement reaches its climax when it is the platforms that move them across the stage, dignified as Apollos or stiff as planks.",
+		link: "",
+		credit: " .",
+	},
 	
 	
 		{
