f818cb90f0c5d15e0ac3d92e586780e8953c4acf
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-09-01 20:37:44 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -386,8 +386,8 @@ let WEBSITE_DATA = [
 	},
 	
 		{
-		name: "Avatar -- Skeleton Mapping",
-		img: "images/Screenshot.png", 
+		name: "Skeletal Avatar",
+		img: "images/9_26194519.png", 
 		blurb: "Iterative studies of motion using motion-capture technology that renders spatial-data as 3D scans and skeletal avatars. The infrared sensor of the kinect affects the sensorial experience of moving.'Skeletal-Avatar: kinect sensor; essentially a camera feed and infrared depth feed, machine learning algorithms that predict a human pose, using openNI - open source, from a public data set, renders a skeleton of a person’s body, adjacent to and insync with their movements.",
 		link: "",
 		credit:"",
