ca5d132e75c57b884627365fb19e4362b0ff2dee
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-14 13:17:04 -0400

    Update website-data.js
    no memory dance

--- website-data.js
+++ website-data.js
@@ -250,13 +250,13 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 
-{
-		name: "Memory Dance (Research)",
-		img: "images/122118212001182120118.gif",
-		blurb: "Iterative studies of motion, using motion-capture and machine learning as proprioceptive mappings. The infrared sensor of the kinect affects the body’s sensorial experience of motion. As a dance teacher, I consider the ways this software could be used by students in a dance class. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? I wonder how that feedback could be effective and productive in learning movement as I'm not relying on ocular relation to create representations, I have to rely on my orientation to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
-		link: "",
-		credit: "",
-	},
+// {
+// 		name: "Memory Dance (Research)",
+// 		img: "images/122118212001182120118.gif",
+// 		blurb: "Iterative studies of motion, using motion-capture and machine learning as proprioceptive mappings. The infrared sensor of the kinect affects the body’s sensorial experience of motion. As a dance teacher, I consider the ways this software could be used by students in a dance class. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? I wonder how that feedback could be effective and productive in learning movement as I'm not relying on ocular relation to create representations, I have to rely on my orientation to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
+// 		link: "",
+// 		credit: "",
+// 	},
 
 	
 	
