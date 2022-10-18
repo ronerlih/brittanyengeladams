cbd2137174da929fad4e0eba69a000ba4999e335
Ron Erlih <ronerlih@gmail.com>
2022-10-18 19:23:55 +0300

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -288,7 +288,7 @@ let WEBSITE_DATA = [
 		img: "images/08851212212510_2189202011994__2912202011522519.gif",
 		blurb: "Given the special situation of lockdown experienced in 2020 due to the pandemic, many wondered, ‘‘Where and how will we gather during a time of confinement?’ Online platforms became commonplace where material could be shared with a global audience to engage in performance and conversations from home. In many ways, these online platforms brought more diverse groups and communities together, and gave institutions the opportunity to ask and receive feedback from their communities.. Topics such as, ‘Are spaces really open to everyone, or who may feel excluded? — What will these institutions be like in the new normal?.",
 		link: "",
-		credit: ",
+		credit: "",
 	}
 ];
 
