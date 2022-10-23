661dac09570f8c2ef2b0a0b8629f813b79e56fd5
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-23 16:01:48 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -303,7 +303,7 @@ let WEBSITE_DATA = [
 	
 		{
 		name: "Future Bodies Exhibition",
-		img: "images/1651351514511820_lt_h.gif",
+		img: "images/The Sirens_by_Joshua Okoro108.jpg",
 		blurb: "Performance of BodyMouth presented by Future Bodies at The New Media Caucus Exhibition on October 1st 2022.",
 		link: "",
 		credit: "",
