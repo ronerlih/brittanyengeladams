bb244a4282f19844af3ccffdefa21a35002e9b86
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-27 03:18:36 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -29,12 +29,12 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 	
-					{
-		name: "Skeletal Avatar",
-		img: "images/814181211418152114181534114181521520.gif",
-		blurb: "Skeleton data. (a) A template skeleton and its joints. The skeleton is a simple directed graph, in which the directions are denoted by the arrows beside the skeleton edges. For denotational simplicity, we do not show the arrows in other figures. (b) Several pairs of input noisy skeletons (upper) and ground truth skeletons (lower).",
+	{
+		name: "YouTube Improv  | Website",
+		img: "images/YouTubeimprovhigh.gif",
+		blurb: "‘YouTube Improv' is an automated website for live performance. The tool produces spontaneity and indeterminacy, while also bringing the authenticity of the real on stage, which depicts local people, real events, media, and culture.",
 		link: "",
-		credit: "",
+		credit: ""
 	},
 			{
 		name: "Maquette 2021",
@@ -74,13 +74,14 @@ let WEBSITE_DATA = [
 		link: "",
 		credit: "",
 	},
-		{
-		name: "YouTube Improv  | Website",
-		img: "images/YouTubeimprovhigh.gif",
-		blurb: "‘YouTube Improv' is an automated website for live performance. The tool produces spontaneity and indeterminacy, while also bringing the authenticity of the real on stage, which depicts local people, real events, media, and culture.",
+						{
+		name: "Skeletal Avatar",
+		img: "images/814181211418152114181534114181521520.gif",
+		blurb: "Skeleton data. (a) A template skeleton and its joints. The skeleton is a simple directed graph, in which the directions are denoted by the arrows beside the skeleton edges. For denotational simplicity, we do not show the arrows in other figures. (b) Several pairs of input noisy skeletons (upper) and ground truth skeletons (lower).",
 		link: "",
-		credit: ""
+		credit: "",
 	},
+	
 	
 	
 		{
