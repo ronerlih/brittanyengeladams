40c87a8f2e218d3a04d9ca9df4944eb2d7dbc214
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-27 02:14:37 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -172,7 +172,7 @@ let WEBSITE_DATA = [
 
 				{
 			name: "Architecture of Memory",
-			img: "images/4113194113194113191375121411319.jpg", 
+			img: "images/2138383815138158.jpg", 
 			blurb: "‘Architecture of Memory’ is an ongoing research project and repository of recollections that places loss and rupture as opportunities for transformation and growth. From MRPJ:It could be theorized that blackness is that experience constituted by a naturalized lack of plot, history, and the ontological fortitude those things convey. This formulation is broached in 'Architecture of Memory' where artist Brittany Engel-Adams uses digital art techniques in order to (re)member ancestral performances. By (re)presenting the gestures that were her own necessary precondition, (i.e.those of her relatives) the artist forms a togetherness that transcends the self as a discreetly positioned thing in space-time and (re)supposes a possibility of experience both contingent upon and in excess to the phenomenal and causal.",
 			link: "",
 			credit: "",
