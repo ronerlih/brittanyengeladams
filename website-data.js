f21bd7f7a6e38715255a002a5648ef0f446b8763
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-26 20:23:47 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -37,14 +37,14 @@ let WEBSITE_DATA = [
 	
 	{
 		name: "Hand Dance | Film",
-		img: "images/111945111814926145119451114619149261914926145.jpg",
+		img: "images/8114114351145115114.jpg",
 		blurb: "'Hand Dance' film, commissioned by, The Guggenheim Museum’s Works & Process, in New York, New York, from 2020. 'Hand Dance' a collaborative work by David Lang, Annie-B Parson, Ron Erlih, Brittany Engel-Adams.",
 		link: "https://www.guggenheim.org/initiatives/works-process/virtual-commissions",
 		credit: "",
 	},
 	
 		{
-		name: "Motion-Capture & A.I. Research",
+		name: "Motion-Capture & Mapping",
 		img: "images/122118212001182120118.gif",
 		blurb: "Proprioceptive research using artificial intelligence and motion capture technology. As a dance teacher, I consider the ways this software could be used by students in a dance class. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? I wonder how that feedback could be effective and productive in learning movement as I'm not relying on ocular relation to create representations, I have to rely on my orientation to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
 		link: "",
