7d51087f040e613e3f72e8eb910adcde7c3286a3
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-27 21:57:55 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -139,14 +139,14 @@ let WEBSITE_DATA = [
 		link: "",
 		credit: ""
 	},
-	
-	{
-		name: "Emergent Landscape II | Tessellation,",
-		img: "images/1266215159166122591612215916612916.png",
-		blurb: "Digital Collage.",
-		link: "https://www.brittanyengeladams.com/who-blue.html",
+	  {
+		name: "Farewell 2020",
+		img: "images/08851212212510_2189202011994__2912202011522519.gif",
+		blurb: "Given the special situation of lockdown experienced in 2020 due to the pandemic, many wondered, ‘Where and how will we gather during a time of confinement?’ Online platforms became commonplace where material could be shared with a global audience to engage in performance and conversations from home. In many ways, these online platforms brought more diverse groups and communities together, and gave institutions the opportunity to ask and receive feedback from their communities.. Topics such as, ‘Are spaces really open to everyone, or who may feel excluded? — What will these institutions be like in the new normal?.",
+		link: "",
 		credit: "",
-	},
+		},
+	
 		{
 		name: "Wireless Fidelity | Exhibition",
 		img: "images/18151613.jpg",
@@ -209,14 +209,14 @@ let WEBSITE_DATA = [
 	
 	
 
-	
-	  {
-		name: "Farewell 2020",
-		img: "images/08851212212510_2189202011994__2912202011522519.gif",
-		blurb: "Given the special situation of lockdown experienced in 2020 due to the pandemic, many wondered, ‘Where and how will we gather during a time of confinement?’ Online platforms became commonplace where material could be shared with a global audience to engage in performance and conversations from home. In many ways, these online platforms brought more diverse groups and communities together, and gave institutions the opportunity to ask and receive feedback from their communities.. Topics such as, ‘Are spaces really open to everyone, or who may feel excluded? — What will these institutions be like in the new normal?.",
-		link: "",
+	{
+		name: "Emergent Landscape II | Tessellation,",
+		img: "images/1266215159166122591612215916612916.png",
+		blurb: "Digital Collage.",
+		link: "https://www.brittanyengeladams.com/who-blue.html",
 		credit: "",
-		},
+	},
+	
 	
 // 	{
 // 		name: "Before Zeta | Film",
