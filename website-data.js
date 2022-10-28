9b7eff1fbfade912ac737846c71a615178b0cbed
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-28 00:58:00 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -140,6 +140,14 @@ let WEBSITE_DATA = [
 		credit: ""
 	},
 	
+				{
+		name: "Memory Dance",
+		img: "images/bh_accretiondisk_sim_stationary_websize.gif",
+		blurb: "‘Memory Dance’ uses an open source library (OpenNI). (OpenNI)' s algorithms create a skeletal-avatar of a body based on a mathematical transformation that turns movement into 3d spatial-data. Using ‘Kinect’ movement-capture recording, and A.I. software that programmatically records and loops movements, shows a live camera feed of the Kinect, and superimposes the movements in the frame of the camera.",
+		link: "",
+		credit: "Photo by Nasa",
+	},
+	
 		{
 			name: "Curator at Rutgers University | Student Dance Concert",
 			img: "images/16101918161297820191816125129782019.jpg", 
@@ -174,13 +182,7 @@ let WEBSITE_DATA = [
 		
 		},
 	
-				{
-		name: "Memory Dance",
-		img: "images/bh_accretiondisk_sim_stationary_websize.gif",
-		blurb: "‘Memory Dance’ uses an open source library (OpenNI). (OpenNI)' s algorithms create a skeletal-avatar of a body based on a mathematical transformation that turns movement into 3d spatial-data. Using ‘Kinect’ movement-capture recording, and A.I. software that programmatically records and loops movements, shows a live camera feed of the Kinect, and superimposes the movements in the frame of the camera.",
-		link: "",
-		credit: "Photo by Nasa",
-	},
+	
 		{
 		name: "Hand Dance | Research",
 		img: "images/1215151151515147229523.png",
