938286dfda079c7f8c12fe8fa2555608f3bce710
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-27 23:34:42 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -1,14 +1,7 @@
 let WEBSITE_DATA = [
 	
 
-			{
-
-		name: "Human Robot",
-		img: "images/22.-recurso_avances-en-robotica-scaled-e1634287575105-1024x691.jpg",
-		blurb: "Is it possible to build artificial consciousness similar to human consciousness in digitized structures? If artificial intelligence will digitally reproduce the artificial structures of neurons, and the entire central nervous systems of humans, then an artificial nervous system counterpoint of cybernetic consciousness could be equivalent to human consciousness, that is capable of collecting and processing data, in a much larger database capacity than what’s possible at present.",
-		link: "",
-		credit: "",
-	},
+	
 	
 
 	
@@ -35,6 +28,16 @@ let WEBSITE_DATA = [
 		link: "",
 		credit: "",
 	},
+	
+			{
+
+		name: "Human Robot",
+		img: "images/22.-recurso_avances-en-robotica-scaled-e1634287575105-1024x691.jpg",
+		blurb: "Is it possible to build artificial consciousness similar to human consciousness in digitized structures? If artificial intelligence will digitally reproduce the artificial structures of neurons, and the entire central nervous systems of humans, then an artificial nervous system counterpoint of cybernetic consciousness could be equivalent to human consciousness, that is capable of collecting and processing data, in a much larger database capacity than what’s possible at present.",
+		link: "",
+		credit: "",
+	},
+	
 	{
 		name: "Proprioceptive Research | Mapping",
 		img: "images/Avatartracking.jpg",
