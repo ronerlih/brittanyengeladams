82b2a00d8d1548f91bf2a1cd3cc8620b948a3af6
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-28 03:13:50 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -236,7 +236,7 @@ let WEBSITE_DATA = [
 	
 		{
 		name: "Emergent Landscape III",
-		img: "images/Ameribanned_leg__cut.gif",
+		img: "images/Who_wood_picture1.png",
 		blurb: "Tessellation, Digital-Collage.",
 		link: "https://www.brittanyengeladams.com/who-wood.html",
 		credit: "",
