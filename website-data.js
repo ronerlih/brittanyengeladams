37d2ed67d7ab18bba8b453e3816bcc4f719beda2
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-28 02:36:16 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -127,7 +127,7 @@ let WEBSITE_DATA = [
 	
 		{
 			name: "Curator at Rutgers University | Student Dance Concert",
-			img: "images/barcodeprogram.JPG", 
+			img: "images/RutgersProgram.jpg", 
 			blurb: "The Concert showcased works that were responses to theatrical productions of the 19th/ 20th century reimagined for the 21st century stage. Presented by Rutgers University.",
 			link: "",
 			credit: "",
