34c55d143093ff9c7a586caf0b94c35ba15ba31b
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-28 04:33:05 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -55,13 +55,13 @@ let WEBSITE_DATA = [
 	},
 	
 	
-	{
-		name: "Wireless Fidelity | Exhibition",
-		img: "images/18151613.jpg",
-		blurb: "Wireless Fidelity presented at The Gallery Thaddaus Ropac in Paris in 2015.",
+		{
+		name: "Proprioceptive Research | Mapping",
+		img: "images/122118212001182120118.gif",
+		blurb: "Proprioceptive research using artificial intelligence and motion capture technology. As a dance teacher, I consider the ways this software could be used by students in a dance class. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? I wonder how that feedback could be effective and productive in learning movement as I'm not relying on ocular relation to create representations, I have to rely on my orientation to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
 		link: "",
 		credit: "",
-	},	
+	},
 	
 		
 	
@@ -106,14 +106,14 @@ let WEBSITE_DATA = [
 		link: "",
 		credit: "",
 	},
-	
-		{
-		name: "Proprioceptive Research | Mapping",
-		img: "images/122118212001182120118.gif",
-		blurb: "Proprioceptive research using artificial intelligence and motion capture technology. As a dance teacher, I consider the ways this software could be used by students in a dance class. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? I wonder how that feedback could be effective and productive in learning movement as I'm not relying on ocular relation to create representations, I have to rely on my orientation to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
+	{
+		name: "Wireless Fidelity | Exhibition",
+		img: "images/18151613.jpg",
+		blurb: "Wireless Fidelity presented at The Gallery Thaddaus Ropac in Paris in 2015.",
 		link: "",
 		credit: "",
-	},
+	},	
+	
 		
 			{
 
