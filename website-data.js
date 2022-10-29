6c72e05e4b3e95c8d4a5266df667887f3d312d02
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-29 12:19:44 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -75,6 +75,15 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 	
+	 {
+		name: "Farewell 2020",
+		img: "images/bh_accretiondisk_sim_stationary_websize.gif",
+		blurb: "Given the special situation of lockdown experienced in 2020 due to the pandemic, many wondered, ‘Where and how will we gather during a time of confinement?’ Online platforms became commonplace where material could be shared with a global audience to engage in performance and conversations from home. In many ways, these online platforms brought more diverse groups and communities together, and gave institutions the opportunity to ask and receive feedback from their communities.. Topics such as, ‘Are spaces really open to everyone, or who may feel excluded? — What will these institutions be like in the new normal?.",
+		link: "",
+		credit:"Visualization by Nasa",
+		},
+	
+	
 		{
 		name: "Shower | Performance",
 		img: "images/5S20221028102648PM.jpg",
@@ -190,13 +199,6 @@ let WEBSITE_DATA = [
 		link: "",
 		credit: "",
 		
-		},
-	 {
-		name: "Farewell 2020",
-		img: "images/bh_accretiondisk_sim_stationary_websize.gif",
-		blurb: "Given the special situation of lockdown experienced in 2020 due to the pandemic, many wondered, ‘Where and how will we gather during a time of confinement?’ Online platforms became commonplace where material could be shared with a global audience to engage in performance and conversations from home. In many ways, these online platforms brought more diverse groups and communities together, and gave institutions the opportunity to ask and receive feedback from their communities.. Topics such as, ‘Are spaces really open to everyone, or who may feel excluded? — What will these institutions be like in the new normal?.",
-		link: "",
-		credit:"Visualization by Nasa",
 		},
 	
 
