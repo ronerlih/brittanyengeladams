36badebd3fc0e7c47b05c84e8d8791f1307db643
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-10-28 22:22:38 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -168,7 +168,7 @@ let WEBSITE_DATA = [
 	},
 	{
 		name: "YouTube Improv  | Website",
-		img: "images/YouTubeimprovhigh.gif",
+		img: "images/YouTubeimprov2.gif",
 		blurb: "‘YouTube Improv' is an automated website for live performance. The tool produces spontaneity and indeterminacy, while also bringing the authenticity of the real on stage, which depicts local people, real events, media, and culture.",
 		link: "",
 		credit: ""
