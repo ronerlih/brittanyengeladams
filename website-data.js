563ed17c0a5663bfb03670d453cfba781294c21d
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-11-02 15:04:26 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -55,7 +55,7 @@ let WEBSITE_DATA = [
 	
 	{
 		name: "Hand Dance | Film",
-		img: "images/",
+		img: "images/8114114351145115114.jpg",
 		blurb: "'Hand Dance' film, commissioned by, The Guggenheim Museum’s Works & Process, in New York, New York, from 2020. 'Hand Dance' a collaborative work by David Lang, Annie-B Parson, Ron Erlih, Brittany Engel-Adams.",
 		link: "https://www.guggenheim.org/initiatives/works-process/virtual-commissions",
 		credit: "",
