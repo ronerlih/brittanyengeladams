60aee669034e4a5d11c114620c85ac71e0bfadd2
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-11-07 21:40:16 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -50,9 +50,18 @@ let WEBSITE_DATA = [
 
 		
 	{
-		name: " Movement Mapping",
+		name: "Movement Mapping",
 		img: "images/22.-recurso_avances-en-robotica-scaled-e1634287575105-1024x691.jpg",
 		blurb: "Proprioceptive research using artificial intelligence and motion capture technology. As a dance teacher, I consider the ways this software could be used by students in a dance class. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? I wonder how that feedback could be effective and productive in learning movement as I'm not relying on ocular relation to create representations, I have to rely on my orientation to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
+		link: "",
+		credit: "",
+	},
+	
+		
+	{
+		name: "Wireless Fidelity | Exhibition",
+		img: "images/8537885385349.jpg",
+		blurb: "Wireless Fidelity presented at The Gallery Thaddaus Ropac in Paris in 2015.",
 		link: "",
 		credit: "",
 	},
@@ -195,14 +204,7 @@ let WEBSITE_DATA = [
 
 	
 
-	
-	{
-		name: "Wireless Fidelity | Exhibition",
-		img: "images/8537885385349.jpg",
-		blurb: "Wireless Fidelity presented at The Gallery Thaddaus Ropac in Paris in 2015.",
-		link: "",
-		credit: "",
-	},
+
 
 		
 									{
