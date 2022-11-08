7f81153f340409869d2c26d8d8a7bea552c701c8
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-11-07 22:14:03 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -50,7 +50,7 @@ let WEBSITE_DATA = [
 
 		
 	{
-		name: "Movement Mapping",
+		name: "Proprioceptive Mapping | Mapping",
 		img: "images/22.-recurso_avances-en-robotica-scaled-e1634287575105-1024x691.jpg",
 		blurb: "Proprioceptive research using artificial intelligence and motion capture technology. As a dance teacher, I consider the ways this software could be used by students in a dance class. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? I wonder how that feedback could be effective and productive in learning movement as I'm not relying on ocular relation to create representations, I have to rely on my orientation to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
 		link: "",
@@ -144,13 +144,13 @@ let WEBSITE_DATA = [
 	
 
 		
-   {
-		name: "YouTube Improv  | Performance",
-		img: "images/72115225215215225591316152255.jpg",
-		blurb: "‘YouTube Improv' plays trending YouTube videos in the venue's proximity. The performers on stage then have to adjust their movement score according to the algorithmic orchestrated playlist as well all do. Presented by Mason Gross School of the Arts at Rutgers University in 2018.",
-		link: "",
-		credit: ""
-	},
+//    {
+// 		name: "YouTube Improv  | Performance",
+// 		img: "images/72115225215215225591316152255.jpg",
+// 		blurb: "‘YouTube Improv' plays trending YouTube videos in the venue's proximity. The performers on stage then have to adjust their movement score according to the algorithmic orchestrated playlist as well all do. Presented by Mason Gross School of the Arts at Rutgers University in 2018.",
+// 		link: "",
+// 		credit: ""
+// 	},
 	
 		{
 		name: "Harvestworks |  Residency",
