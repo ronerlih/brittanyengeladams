13ca0e65170a40b366be6c94548b821daa4b8033
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-11-09 00:27:34 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -56,6 +56,14 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 	
+			{
+		name: "Maquette 2021",
+		img: "images/0119191215191919.png",
+		blurb: "A robot called the 'maquette' which is a prototype. Inspired by Walter Benjamin's “The Work of Art in the Age of Mechanical Reproduction”, and his concept of the ‘aura’, to describe how we use the machine in order to make art, is in a sense a requiem to handmade art. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork, and the uniqueness that is deprived through the mechanical reproduction of it.", 
+		link: "",
+		credit: "",
+	},
+	
 		{
 		name: "Digital Sculpture",
 		img: "images/11811_192114417.gif",
@@ -64,13 +72,7 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 	
-			{
-		name: "Maquette 2021",
-		img: "images/0119191215191919.png",
-		blurb: "A robot called the 'maquette' which is a prototype. Inspired by Walter Benjamin's “The Work of Art in the Age of Mechanical Reproduction”, and his concept of the ‘aura’, to describe how we use the machine in order to make art, is in a sense a requiem to handmade art. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork, and the uniqueness that is deprived through the mechanical reproduction of it.", 
-		link: "",
-		credit: "",
-	},
+	
 	
 			{
 		name: "Emergent Landscape I | Tessellation",
