51472def6d19bc04b25f9d6bde6d81cfa228ccb8
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-11-08 21:03:25 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -47,6 +47,15 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 		
+	
+	{
+		name: "Wireless Fidelity | Exhibition",
+		img: "images/8537885385349.jpg",
+		blurb: "Wireless Fidelity presented at The Gallery Thaddaus Ropac in Paris in 2015.",
+		link: "",
+		credit: "",
+	},
+	
 			{
 		name: "Emergent Landscape I | Tessellation  ",
 		img: "images/11_91919_.jpg",
@@ -55,13 +64,6 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 		
-	{
-		name: "Wireless Fidelity | Exhibition",
-		img: "images/8537885385349.jpg",
-		blurb: "Wireless Fidelity presented at The Gallery Thaddaus Ropac in Paris in 2015.",
-		link: "",
-		credit: "",
-	},
 	
 			{
 		name: "Emergent Landscape II | Tessellation,",
@@ -232,13 +234,7 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 	
-		{
-		name: "Proprioceptive Research | Mapping",
-		img: "images/118__111118___111492__614_91949_2614919.gif",
-		blurb: "Proprioceptive research using artificial intelligence and motion capture technology. As a dance teacher, I consider the ways this software could be used by students in a dance class. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? I wonder how that feedback could be effective and productive in learning movement as I'm not relying on ocular relation to create representations, I have to rely on my orientation to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
-		link: "",
-		credit: "",
-	},
+
 
 	{
 			name: "Architecture of Memory",
@@ -248,7 +244,13 @@ let WEBSITE_DATA = [
 			credit: "",
 		},
 	
-	
+			{
+		name: "Proprioceptive Research | Mapping",
+		img: "images/118__111118___111492__614_91949_2614919.gif",
+		blurb: "Proprioceptive research using artificial intelligence and motion capture technology. As a dance teacher, I consider the ways this software could be used by students in a dance class. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? I wonder how that feedback could be effective and productive in learning movement as I'm not relying on ocular relation to create representations, I have to rely on my orientation to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
+		link: "",
+		credit: "",
+	},
 	
 	
 	
