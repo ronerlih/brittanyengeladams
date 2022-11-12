931188e4778a33ab291d6850bdea1916829053c7
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-11-12 11:37:29 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -48,13 +48,7 @@ let WEBSITE_DATA = [
 	},
 
 
-		{
-		name: "Wireless Fidelity | Exhibition",
-		img: "images/8537885385349.jpg",
-		blurb: "Wireless Fidelity presented at The Gallery Thaddaus Ropac in Paris in 2015.",
-		link: "",
-		credit: "",
-	},
+	
 					{
 		name: "Emergent Landscape I | Tessellation",
 		img: "images/11_91919_.jpg",
@@ -62,6 +56,14 @@ let WEBSITE_DATA = [
 		link: "https://www.brittanyengeladams.com/red-who.html",
 		credit: "",
 	},	
+	
+		{
+		name: "Wireless Fidelity | Exhibition",
+		img: "images/8537885385349.jpg",
+		blurb: "Wireless Fidelity presented at The Gallery Thaddaus Ropac in Paris in 2015.",
+		link: "",
+		credit: "",
+	},
 	
 		{
 		name: "Digital Sculpture",
