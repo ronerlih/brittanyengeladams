88acb9b090cb0b287e6326b2cf22ac8595048c64
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-11-18 13:08:01 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -14,10 +14,10 @@ let WEBSITE_DATA = [
 	
 		{
 		name: "BodyMouth/Ielele | Symposium",
-		img: "images/BodyMouth_FutureBodies.gif",
+		img: "images/Futurebodies_by_JoshuaOkoro108.jpg",
 		blurb: "'BodyMouth/Ielele presented  at The Future Bodies – New Media Caucus Symposium 2022, in Blacksburg, at Virginia Tech.",
 		link: "",
-		credit: "",
+		credit: "Photo by Joshua Okoro",
 		
 		},
 	
