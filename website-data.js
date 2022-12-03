8c1e8470ead340c70eb44be86c018580e61b8456
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-12-03 08:39:46 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -39,7 +39,7 @@ let WEBSITE_DATA = [
 	
 				{
 		name: "Maquette 2021",
-		img: "images/adolph_men_zel123.png",
+		img: "images/11842191415_218514201184.jpg",
 		blurb: "A robot called the 'maquette' which is a prototype. Inspired by Walter Benjamin's “The Work of Art in the Age of Mechanical Reproduction”, and his concept of the ‘aura’, to describe how we use the machine in order to make art, is in a sense a requiem to handmade art. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork, and the uniqueness that is deprived through the mechanical reproduction of it.", 
 		link: "",
 		credit: "",
