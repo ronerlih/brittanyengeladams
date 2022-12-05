abcd1dcc718b89c50556b35f80e6d0a7ceab0dce
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-12-05 01:16:48 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -31,7 +31,7 @@ let WEBSITE_DATA = [
 
 											{
 		name: "Skeletal Avatar",
-		img: "images/avtarhigh.png",
+		img: "images/11842191415_218514201184.jpg",
 		blurb: "Skeleton data. (a) A template skeleton and its joints. The skeleton is a simple directed graph, in which the directions are denoted by the arrows beside the skeleton edges. For denotational simplicity, we do not show the arrows in other figures. (b) Several pairs of input noisy skeletons (upper) and ground truth skeletons (lower).",
 		link: "",
 		credit: "",
@@ -39,7 +39,7 @@ let WEBSITE_DATA = [
 	
 				{
 		name: "Maquette 2021",
-		img: "images/11842191415_218514201184.jpg",
+		img: "images/0119191215191919.jpg",
 		blurb: "A robot called the 'maquette' which is a prototype. Inspired by Walter Benjamin's “The Work of Art in the Age of Mechanical Reproduction”, and his concept of the ‘aura’, to describe how we use the machine in order to make art, is in a sense a requiem to handmade art. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork, and the uniqueness that is deprived through the mechanical reproduction of it.", 
 		link: "",
 		credit: "",
