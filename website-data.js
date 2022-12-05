c2fc5e9c00d95d6fcdc9aaeed36cf78846347fdf
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-12-05 00:42:08 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -300,7 +300,7 @@ let WEBSITE_DATA = [
 	
 // 		{
 // 		name: "I am not a Robot | 2022",
-// 		img: "images/11414121191918.gif",
+// 		img: "images/1233740.gif",
 // 		blurb: "Adversarial Neural Networks, and Machine Learning Algorithms. A performer performs a series of tasks, as a set of instructions, that an avatar simulation is trying to learn and perfect.",
 // 		link: "",
 // 		credit: "",
