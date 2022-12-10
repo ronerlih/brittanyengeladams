c0b740613cd436c905513a6f2ede5ce8dbfa00bd
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-12-10 01:51:55 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -179,7 +179,7 @@ let WEBSITE_DATA = [
 	},
 		{
 		name: "Emergent Landscape II | Tessellation",
-		img: "images/231519115195212215193152121.gif",
+		img: "images/1266215159166122591612215916612916.png",
 		blurb: "Social Media is a black box and echo chamber, the result of algorithmic dynamics aided by opaque interfaces caused by the homogenization of friend recommendations and personal news feeds, often referred to as online “homophily,” or colloquially as the ‘birds of a feather flock together’ phenomenon.",
 		link: "https://www.brittanyengeladams.com/who-blue.html",
 		credit: "",
