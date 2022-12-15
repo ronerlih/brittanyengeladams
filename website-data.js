d43f721c123d98b1ded260131d4ec4b531b52fd0
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-12-14 23:32:09 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -40,7 +40,7 @@ let WEBSITE_DATA = [
 	
 			{
 		name: "I Am Not A Robot",
-		img: "images/1233740.gif",
+		img: "images/814181211418152114181534114181521520.gif",
 		blurb: "Adversarial Neural Networks & Cumulative Machine Learning Algorithms. A human performs a series of tasks, as a set of instructions that an avatar simulation is trying to learn and perfect, exploring the interplay between human actions, ecosystems, and the systems that unite them.",
 		link: "",
 		credit: "",
