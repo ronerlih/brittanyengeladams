39c31cc680cf00a772f47d8e5ea24435d4b65b05
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-12-15 15:38:19 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -9,7 +9,7 @@ let WEBSITE_DATA = [
 		{
 		name: "BodyMouth/Ielele | Symposium",
 		img: "images/SS20221215at11339AM.png",
-		blurb: "'BodyMouth 'Ielele' at The New Media Caucus Symposium that took place from September 30th-October 2nd 2022 at Virginia Tech and was focused on the theme of 'Future Bodies.' The New Media Caucus (NMC) is an international non-profit association formed to promote the development and understanding of new media art. ",
+		blurb: "'BodyMouth 'Ielele' at The New Media Caucus Symposium, September 30th-October 2nd 2022, at Virginia Tech, focused on the theme of 'Future Bodies.' The New Media Caucus (NMC) is an international non-profit association formed to promote the development and understanding of new media art.",
 		link: "https://futurebodies.newmediacaucus.org/",
 		credit: "Photo by Rodney Kimbangu",
 		
