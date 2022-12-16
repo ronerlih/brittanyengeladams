a5f1558fd5e537f7ac9f09eb31d15dfeac43db44
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-12-16 12:37:39 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -243,7 +243,7 @@ let WEBSITE_DATA = [
 	},
 	{
 			name: "Joffrey Ballet School (Faculty)",
-			img: "images/4351435143514354351435_1.png",
+			img: "images/Joffrey61855551311419380012School.png",
 			blurb: "(2012-2017) Faculty at the Joffrey Ballet School. ‘Robert Joffrey and Gerald Arpino founded the Joffrey Ballet School in 1953. Robert Joffrey and Gerald Arpino founded the Joffrey Ballet School in 1953. The school continues to strive for its founders’ vision by deconstructing dance to allow dancers the ability to perfect their technique and form so it flows naturally and passionately from within. Joffrey Ballet School cultivates the individual artist within you. continues to strive for its founders’ vision by deconstructing dance to allow dancers the ability to perfect their technique and form so it flows naturally and passionately from within. Joffrey Ballet School cultivates the individual artist within you.’",
 			link: "https://www.joffreyballetschool.com",
 			credit: "",
