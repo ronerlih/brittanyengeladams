1689d782ba727c5ffbdfae19e749fe08aedec235
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2022-12-24 18:06:33 +0200

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -251,21 +251,18 @@ let WEBSITE_DATA = [
 	},	
 
 	
-
-
-	
-		{
-		name: "Archival Artifact 'Mizrach'",
-		img: "images/1926318892613892618138.jpg",
-		blurb: "A 'Mizrach' crafted by  my great-great grandfather, Israel Manaschewitz for my grandmother, Judy Engel, whose from the lineage of one of the most renowned scholars of his day, Rabbi Ezekiel Landau, known by his book, 'Noda be-Yehuda'. He is believed to be a descendant of the famous commentator of the 12th century, Rashi.",
-		link: "",
-		credit: "",
-	},
 					{
 		name: "Hand Dance | Film",
 		img: "images/8114114351145115114.png",
 		blurb: "‘Hand Dance’ is a film by Brittany Engel-Adams made in collaboration with David Lang, Annie-B Parson and Ron Erlih for The Guggenheim Museum’s Works & Process. ‘Hand Dance’ uses a hand pose estimation algorithm to overlay the original hands from a dataset on a performer. The algorithm used to ‘learn’ how to estimate a hand pose, perhaps is emancipating those anonymous hands, or at least giving them a place in the foreground suggesting a larger world beyond the performer’s home.",
 		link: "https://www.guggenheim.org/initiatives/works-process/virtual-commissions",
+		credit: "",
+	},
+			{
+		name: "Archival Artifact 'Mizrach'",
+		img: "images/1926318892613892618138.jpg",
+		blurb: "A 'Mizrach' crafted by  my great-great grandfather, Israel Manaschewitz for my grandmother, Judy Engel, whose from the lineage of one of the most renowned scholars of his day, Rabbi Ezekiel Landau, known by his book, 'Noda be-Yehuda'. He is believed to be a descendant of the famous commentator of the 12th century, Rashi.",
+		link: "",
 		credit: "",
 	},
 	
