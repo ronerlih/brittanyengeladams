0914aed12b009675323b97ff3834df6667519274
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-01-03 15:30:06 +0200

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -43,7 +43,7 @@ let WEBSITE_DATA = [
 
 		{
 		name: "Memory Dance",
-		img: "images/1221251201515018195125201514.gif",
+		img: "images/tensorflow.gif",
 		blurb: "‘Memory Dance’ uses infrared sensor of kinect, motion capture technology, and turns the human body into 3d spatial-data,  records and loops movements (similar to a musical looper), shows a live camera feed of the recording, and superimposes the movements in the frame of the camera.",
 		link: "",
 		credit: "",
@@ -234,7 +234,7 @@ let WEBSITE_DATA = [
 	},
 										{
 		name: "Proprioceptive Research | Mapping",
-		img: "images/tensorflow.gif",
+		img: "images/1221251201515018195125201514.gif",
 		blurb: "Proprioceptive research using artificial intelligence and motion capture technology. As a dance teacher, I consider the ways this software could be used by students in a dance class. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? I wonder how that feedback could be effective and productive in learning movement as I'm not relying on ocular relation to create representations, I have to rely on my orientation to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
 		link: "",
 		credit: "",
