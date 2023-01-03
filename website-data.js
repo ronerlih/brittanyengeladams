04e770b42b50ae2379e6ca89cb32c08480a4a3bd
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-01-03 14:37:20 +0200

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -253,7 +253,7 @@ let WEBSITE_DATA = [
 	},	
 		{
 		name: "Cape Cod Midcentury Architecture  | Artist Residency",
-		img: "images/capebook.png",
+		img: "images/capecape.png",
 		blurb: "Brittany Engel-Adams presented two pieces that she worked on while in Wellfleet: 'Architecture of Memory', published in Movement Research Performance Journal No. 55; and 'Hand Dance', a dance film commissioned by the Guggenheim Works & Process. The four artists from the 2020 Cape Cod Modern House Trust Residency have work published in the Digital Guestbook.",
 		link: "https://ccmht.org/",
 		credit: "",
