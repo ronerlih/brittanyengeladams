65fcb1c1fff68add58dfdd920f262c45caa9800c
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-01-08 18:06:06 +0200

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -130,7 +130,7 @@ let WEBSITE_DATA = [
 
 		{
 		name: "Hand Dance | Research",
-		img: "images/11414121191918.gif",
+		img: "images/1215151151515147229523.png",
 		blurb: "‘Displaced figure’ that programmatically transposes different body parts and overlays them on top of the footage as if disintegrating into the background of the scene.",
 		link: "",
 		credit: "",
