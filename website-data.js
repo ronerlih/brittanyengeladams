1162b75dc14bacb94f420456af4b3f002bb3db08
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-01-30 08:50:18 +0100

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -98,7 +98,7 @@ let WEBSITE_DATA = [
 
 	{
 		name: "Maquette 21",
-		img: "images/Avatartracking.jpeg",
+		img: "images/bostond.gif",
 		blurb: "The Maquette is a robotic automaton, designed for a performative work, inspired by Choreographer Deborah Hay's 1966 work ‘Solo’ that is not really a solo but rather a work for 16 dancers, 8 tele-guided platforms and their operators, each dancer follows a solitary path that only episodically cross. Their ecology of movement reaches its climax when it is the platforms that move them across the stage, dignified as Apollos or stiff as planks.", 
 		link: "",
 		credit: "",
