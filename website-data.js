e001f667e38c2c4da91b16e37a091f97546c3e03
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-03 15:51:39 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -129,7 +129,7 @@ let WEBSITE_DATA = [
 
 																				{
 		name: "Proprioceptive Research | Mapping",
-		img: "images/arduino-breadboard-3D_DHQ.jpg",
+		img: "images/1233740.gif",
 		blurb: "Proprioceptive research using artificial intelligence and motion capture technology. As a Dance Teacher, I consider the ways this software could be used for dance training. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? That feedback could be effective and productive in learning movement, as I'm not relying on ocular relation to create representations, I have to rely on an orientation  to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
 		link: "",
 		credit: "",
@@ -156,7 +156,7 @@ let WEBSITE_DATA = [
 	
 	{
 		name: "Human Robot",
-		img: "images/1233740.gif",
+		img: "images/arduino-breadboard-3D_DHQ.jpg",
 		blurb: "Adversarial Neural Networks & Machine Learning Algorithms. A robot called the 'maquette' which is a prototype. Inspired by Walter Benjamin's “The Work of Art in the Age of Mechanical Reproduction”, and his concept of the ‘aura’, to describe how we use the machine in order to make art, is in a sense a requiem to handmade art. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork, and the uniqueness that is deprived through the mechanical reproduction of it.", 
 		link: "",
 		credit: "",
