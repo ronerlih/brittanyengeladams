f24cd638fd2fba3b983e52e88d6e3aae0d7d83eb
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-03 19:28:36 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -50,7 +50,7 @@ let WEBSITE_DATA = [
 		},	
  {
 			name: "New York University's Tisch School of the Arts (Faculty)",
-			img: "images/SS_553PM.jpeg",
+			img: "images/ScreenShot2023-01-12at44329PM.png",
 			blurb: "(2020-2022) Faculty in the Dance and Theater Department at New York University's Tisch School of the Arts. ‘Our breadth of excellence across the departments is unique and world-renowned. You can earn a degree in a wide range of disciplines that are uniquely integrated within one school. Artists and scholars come from around the world to study acting, dance, cinema studies, collaborative arts, design for stage and film, dramatic writing, film and television, game design, interactive media arts, interactive telecommunications, moving image archiving and preservation, musical theatre writing, performance studies, photography, public policy, and recorded music.’",
 			link: "https://tisch.nyu.edu/dance/courses",
 			credit: "",
@@ -86,7 +86,7 @@ let WEBSITE_DATA = [
 	
 															{
 		name: "I Am Not A Robot",
-		img: "images/SS7.47.47M.png",
+		img: "images/coggearsmechanismbrassc4dmodel001.jpeg",
 		blurb: "Adversarial Neural Networks & Cumulative Machine Learning Algorithms. A human performs a series of tasks, as a set of instructions that an avatar simulation is trying to learn and perfect, exploring the interplay between human actions, ecosystems, and the systems that unite them.",
 		link: "",
 		credit: "",
