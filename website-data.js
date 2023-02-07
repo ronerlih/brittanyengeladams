0478262db3748a9f1c5207e7173b1288c9569417
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-07 18:05:09 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -391,7 +391,7 @@ let WEBSITE_DATA = [
 	},
 			 {
 		name: "Postmodernism",
-		img: "images/01.jpeg",
+		img: "images/ScreenShot2023-02-03at6.53.06PM.png",
 		blurb:  "As we look forward toward the new impossible challenges of the 21st century. The 21st century will be full of new even-more difficult challenges than we have ever encountered before. Ranging from climate change, to the rise of disruptive technologies like artificial intelligence. The most complicated challenge of all, is the challenge to our humanity. In the coming decades, the twin revolution of bio-tech and info-techo merge into a singular overwhelming scientific tsunami. This might undermine our conception of humanity and  shake the humanist foundation of modernist civilization.",
 		link: "",
 		credit: "",
@@ -428,7 +428,7 @@ let WEBSITE_DATA = [
 	
 	{
 		name: "Singularity",
-		img: "images/ScreenShot2023-02-03at6.53.06PM.png",
+		img: "images/01.jpeg",
 		blurb: "In the world of artificial intelligence, the idea of (singularity) looms large. This slippery concept describes the moment AI exceeds beyond human control and rapidly transforms society. The tricky thing about AI singularity (and why it borrows terminology from black hole physics) is that it’s enormously difficult to predict where it begins and nearly impossible to know what’s beyond this technological (event horizon.) However, some AI researchers are on the hunt for signs of reaching singularity measured by AI progress approaching the skills and ability comparable to a human. One such metric, defined by Translated, a Rome-based translation company, is an AI’s ability to translate speech at the accuracy of a human. Language is one of the most difficult AI challenges, but a computer that could close that gap could theoretically show signs of Artificial General Intelligence (AGI).",
 		link: "",
 		credit: "",
