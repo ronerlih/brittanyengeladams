8ecd5dd18dc1f575f65c624ef5fe9d9b9254e613
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-10 00:42:59 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -82,7 +82,7 @@ let WEBSITE_DATA = [
 	
 						 {
 		name: "Postmodernism",
-		img: "images/ScreenShot2023-02-08at6.14.49PM.png",
+		img: "images/liioncellthumbnail.jpg",
 		blurb:  "As we look forward toward the new impossible challenges of the 21st century. The 21st century will be full of new even-more difficult challenges than we have ever encountered before. Ranging from climate change, to the rise of disruptive technologies like artificial intelligence. The most complicated challenge of all, is the challenge to our humanity. In the coming decades, the twin revolution of bio-tech and info-techo merge into a singular overwhelming scientific tsunami. This might undermine our conception of humanity and  shake the humanist foundation of modernist civilization.",
 		link: "",
 		credit: "",
@@ -116,7 +116,7 @@ let WEBSITE_DATA = [
 	
 	{
 			name: "Architecture of Memory",
-			img: "images/liioncellthumbnail.jpg", 
+			img: "images/_m0t_petty123.png", 
 			blurb: "‘’Architecture of Memory' is a research project that places loss and rupture as opportunity for growth and transformation. Inspired by American Writer Saidiya Hartman's literary device 'Critical Fabulation’ a style of creative semi-nonfiction that attempts to bring the suppressed voices of the past to the surface. Published in The Movement Research Performance Journal No. 55.",
 			link: "",
 			credit: "",
