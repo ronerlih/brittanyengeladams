9018f74eacdad3361330261bd12b66a6e6f58629
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-11 14:09:43 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -92,7 +92,7 @@ let WEBSITE_DATA = [
 	
 														{
 		name: "I Am Not A Robot",
-		img: "images/814181211418152114181534114181521520.gifg",
+		img: "images/814181211418152114181534114181521520.gif",
 		blurb: "Adversarial Neural Networks & Cumulative Machine Learning Algorithms. A human performs a series of tasks, as a set of instructions that an avatar simulation is trying to learn and perfect, exploring the interplay between human actions, ecosystems, and the systems that unite them.",
 		link: "",
 		credit: "",
@@ -430,7 +430,7 @@ let WEBSITE_DATA = [
 	
 		{
 		name: "Singularity",
-		img: "images/ScreenShot2023-02-03at6.53.06PM.png",
+		img: "images/ScreenShot2023-02-11at2.05.14 PM.png",
 		blurb: "In the world of artificial intelligence, the idea of (singularity) looms large. This slippery concept describes the moment AI exceeds beyond human control and rapidly transforms society. The tricky thing about AI singularity (and why it borrows terminology from black hole physics) is that it’s enormously difficult to predict where it begins and nearly impossible to know what’s beyond this technological (event horizon.) However, some AI researchers are on the hunt for signs of reaching singularity measured by AI progress approaching the skills and ability comparable to a human. One such metric, defined by Translated, a Rome-based translation company, is an AI’s ability to translate speech at the accuracy of a human. Language is one of the most difficult AI challenges, but a computer that could close that gap could theoretically show signs of Artificial General Intelligence (AGI).",
 		link: "",
 		credit: "",
