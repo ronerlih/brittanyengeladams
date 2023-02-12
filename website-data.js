bea8e1bcc8de421f1a959a102410bddebbcab792
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-12 18:08:44 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -64,14 +64,7 @@ let WEBSITE_DATA = [
 		}, 	
 	
 	
-	
-																		{
-		name: "Subtractive Synthesis",
-		img: "images/ScreenShot2023-02-07at5.56.29PM.png",
-		blurb: "An Oscillator is used to generate a suitably bright sound. This is routed through a Filter. A Filter is used to cut-off or cut-down the brightness to something more suitable. This resultant sound is routed to an Amplifier. An Amplifier is used to control the loudness of the sound over a period of time so as to emulate a natural instrument.",
-		link: "",
-		credit: "",
-	},
+
 	
 			{
 		name: "Maquette 21",
@@ -82,7 +75,14 @@ let WEBSITE_DATA = [
 	},
 	
 
-
+	
+																		{
+		name: "Subtractive Synthesis",
+		img: "images/ScreenShot2023-02-07at5.56.29PM.png",
+		blurb: "An Oscillator is used to generate a suitably bright sound. This is routed through a Filter. A Filter is used to cut-off or cut-down the brightness to something more suitable. This resultant sound is routed to an Amplifier. An Amplifier is used to control the loudness of the sound over a period of time so as to emulate a natural instrument.",
+		link: "",
+		credit: "",
+	},
 
 	
 	
