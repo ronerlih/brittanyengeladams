98de8977cacc43551d255f468fe91ddf1b222d21
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-11 23:16:46 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -288,14 +288,15 @@ let WEBSITE_DATA = [
 		link: "",
 		credit: "",
 	},
-
-		{
-		name: "Digital Sculpture",
-		img: "images/3dscene.gif",
-		blurb: "The motion capture technology pictographically motions itself, while the possible movements make a kind of digital skin, a boundary of data, that moves with the body, and can be moved through like atoms and fragments.",
+											 		{
+		name: "Superposition",
+		img: "images/t2_026.jpeg",
+		blurb: "Superposition is the potential of an electron to be in a few positions inside of an atom. Superposition deals with a range of the potential of an electron. Meaning, it can exist in two places at the same time. For example, in that, an electron can be both excited – and not excited. When one measures the energy of the electron cloud of an atom, then the cloud of potential collapses into one solution.",
 		link: "",
 		credit: "",
-	},	
+	},
+
+	
 
 
 	
@@ -332,15 +333,15 @@ let WEBSITE_DATA = [
 // 		link: "",
 // 		credit: "",
 // 	},
-										 		{
-		name: "Superposition",
-		img: "images/t2_026.jpeg",
-		blurb: "Superposition is the potential of an electron to be in a few positions inside of an atom. Superposition deals with a range of the potential of an electron. Meaning, it can exist in two places at the same time. For example, in that, an electron can be both excited – and not excited. When one measures the energy of the electron cloud of an atom, then the cloud of potential collapses into one solution.",
+
+		
+			{
+		name: "Digital Sculpture",
+		img: "images/3dscene.gif",
+		blurb: "The motion capture technology pictographically motions itself, while the possible movements make a kind of digital skin, a boundary of data, that moves with the body, and can be moved through like atoms and fragments.",
 		link: "",
 		credit: "",
-	},
-		
-			
+	},		
 		
 				
 																					{
