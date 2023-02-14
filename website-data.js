a0e0bb24ee79d4889427e8b8ce04d547e809b8f8
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-13 23:12:04 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -205,7 +205,7 @@ let WEBSITE_DATA = [
 
 						 {
 		name: "Postmodernism",
-		img: "images/Avatartracking.jpeg",
+		img: "images/242714982_10222276480233848_7374378084717392719_n.jpg",
 		blurb:  "As we look forward toward the new impossible challenges of the 21st century. The 21st century will be full of new even-more difficult challenges than we have ever encountered before. Ranging from climate change, to the rise of disruptive technologies like artificial intelligence. The most complicated challenge of all, is the challenge to our humanity. In the coming decades, the twin revolution of bio-tech and info-techo merge into a singular overwhelming scientific tsunami. This might undermine our conception of humanity and  shake the humanist foundation of modernist civilization.",
 		link: "",
 		credit: "",
