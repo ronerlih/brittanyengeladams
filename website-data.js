c182e90f5b3dd02db23adf7804e0166445afff62
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-16 14:02:33 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -219,7 +219,7 @@ let WEBSITE_DATA = [
 
 													 		{
 		name: "Superposition",
-		img: "images/ScreenShot2023-02-05at2.27.19PM.png",
+		img: "images/ScreenShot2023-02-04at1.48.02PM.png",
 		blurb: "Superposition is the potential of an electron to be in a few positions inside of an atom. Superposition deals with a range of the potential of an electron. Meaning, it can exist in two places at the same time. For example, in that, an electron can be both excited – and not excited. When one measures the energy of the electron cloud of an atom, then the cloud of potential collapses into one solution.",
 		link: "",
 		credit: "",
