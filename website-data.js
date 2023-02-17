1c834f3bea340c878c772c3dfc3eae1df08b0899
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-16 21:31:27 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -169,14 +169,15 @@ let WEBSITE_DATA = [
 		credit: "",
 	},
 
-					 												{
-		name: "Autonomous",
-		img: "images/ScreenShot2023-02-04at1.48.02PM.png",
-		blurb: "Forces of the world allow for a basic interpretations of freewill. In an economic-capitalist sphere freewill represents a sublime force that manifests through desire. This is not freewill. Freewill is not the sublime, sacred idea that pops into your head. It’s not something you have, it’s something struggled for.",
-		link: "",
+		
+													{
+	      name: "Emergent Landscape I | Tessellation",
+		img: "images/11_91919_.jpeg",
+		blurb:  "Online Tessellation",
+		link: "https://www.brittanyengeladams.com/red-who.html",
 		credit: "",
-	},	
-
+	
+	},
 
 
 		 													{
@@ -300,17 +301,16 @@ let WEBSITE_DATA = [
 
 
 	
-		
-													{
-	      name: "Emergent Landscape I | Tessellation",
-		img: "images/11_91919_.jpeg",
-		blurb:  "Online Tessellation",
-		link: "https://www.brittanyengeladams.com/red-who.html",
+
+	
+						 												{
+		name: "Autonomous",
+		img: "images/ScreenShot2023-02-04at1.48.02PM.png",
+		blurb: "Forces of the world allow for a basic interpretations of freewill. In an economic-capitalist sphere freewill represents a sublime force that manifests through desire. This is not freewill. Freewill is not the sublime, sacred idea that pops into your head. It’s not something you have, it’s something struggled for.",
+		link: "",
 		credit: "",
-	
-	},
-	
-	
+	},	
+
 	
 
 
