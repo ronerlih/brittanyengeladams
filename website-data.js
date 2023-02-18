1405dddab7206352edc74ecd41e7a748424fe2d1
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-18 12:03:21 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -40,7 +40,7 @@ let WEBSITE_DATA = [
 	
 				 												{
 		name: "Autonomous",
-		img: "images/SS7.47.47M.png",
+		img: "images/SS2023-02-07at5.45.35PM.png",
 		blurb: "Forces of the world allow for a basic interpretations of freewill. In an economic-capitalist sphere freewill represents a sublime force that manifests through desire. This is not freewill. Freewill is not the sublime, sacred idea that pops into your head. It’s not something you have, it’s something struggled for.",
 		link: "",
 		credit: "",
@@ -148,7 +148,7 @@ let WEBSITE_DATA = [
 	
 {
 			name: "New York University's Tisch School of the Arts",
-			img: "images/ScreenShot2023-01-12at44329PM.png",
+			img: "images/SS_553PM.jpeg",
 			blurb: "(2020-2022) Faculty in the Dance and Theater Department at New York University's Tisch School of the Arts. ‘Our breadth of excellence across the departments is unique and world-renowned. You can earn a degree in a wide range of disciplines that are uniquely integrated within one school. Artists and scholars come from around the world to study acting, dance, cinema studies, collaborative arts, design for stage and film, dramatic writing, film and television, game design, interactive media arts, interactive telecommunications, moving image archiving and preservation, musical theatre writing, performance studies, photography, public policy, and recorded music.’",
 			link: "https://tisch.nyu.edu/dance/courses",
 			credit: "",
@@ -174,7 +174,13 @@ let WEBSITE_DATA = [
 			credit: "",
 		}, 
 
-
+			{
+		name: "Electron Cloud Atomic Model",
+		img: "images/SS7.47.47M.png",
+		blurb: "The fan pictured here is turned off in the photo on the left and running at high speed in the photo on the right. In the right-hand photo, the blades are moving too fast for you to see the individual blades. You can’t tell where any given blade is at any given moment. In some ways, rapidly moving fan blades are similar to electrons moving about the nucleus of an atom. Like fan blades, electrons move very quickly and we can never tell exactly where they are. If that’s the case, how can we represent electrons in models of the atom?.",
+		link: "",
+		credit: "",
+	},
 
 	
 	
@@ -308,13 +314,7 @@ let WEBSITE_DATA = [
 	},	
 	
 
-// 			{
-// 		name: "Electron Cloud Atomic Model",
-// 		img: "images/SS20230122at7.16.09PM.png",
-// 		blurb: "The fan pictured here is turned off in the photo on the left and running at high speed in the photo on the right. In the right-hand photo, the blades are moving too fast for you to see the individual blades. You can’t tell where any given blade is at any given moment. In some ways, rapidly moving fan blades are similar to electrons moving about the nucleus of an atom. Like fan blades, electrons move very quickly and we can never tell exactly where they are. If that’s the case, how can we represent electrons in models of the atom?.",
-// 		link: "",
-// 		credit: "",
-// 	},
+
 	
 						{
 		name: "Ancestral Artifact  'Mizrach'",
