27000561efdf17a924046bcc7757250bbea7d09e
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-22 19:36:56 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -463,7 +463,7 @@ let WEBSITE_DATA = [
 
 	{
 		name: "Substance Dualism",
-		img: "images/ScreenShot2023-02-22at5.22.58PM.png",
+		img: "images/ScreenShot2023-02-22at7.29.50PM.png",
 		blurb: "Substance dualism is also often dubbed ‘Cartesian dualism’, but some substance dualists are keen to distinguish their theories from Descartes’s. E. J. Lowe, for example, is a substance dualist, in the following sense. He holds that a normal human being involves two substances, one a body and the other a person. The latter is not, however, a purely mental substance that can be defined in terms of thought or consciousness alone, as Descartes claimed. But persons and their bodies have different identity conditions and are both substances, so there are two substances essentially involved in a human being, hence this is a form of substance dualism. Lowe (2006) claims that his theory is close to P. F. Strawson’s (1959), whilst admitting that Strawson would not have called it substance dualism.",
 		link: "",
 		credit: "",
