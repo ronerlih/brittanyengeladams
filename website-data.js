91a3f91b9b7f8f99e731d43e27bdc81b5faf803c
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-02-27 17:43:28 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -446,7 +446,7 @@ let WEBSITE_DATA = [
 	
 {
 			name: "New York University's Tisch School of the Arts",
-			img: "images/SS_553PM.jpeg",
+			img: "images/ScreenShot2023-01-12at44329PM.png",
 			blurb: "(2020-2022) Faculty in the Dance and Theater Department at New York University's Tisch School of the Arts. ‘Our breadth of excellence across the departments is unique and world-renowned. You can earn a degree in a wide range of disciplines that are uniquely integrated within one school. Artists and scholars come from around the world to study acting, dance, cinema studies, collaborative arts, design for stage and film, dramatic writing, film and television, game design, interactive media arts, interactive telecommunications, moving image archiving and preservation, musical theatre writing, performance studies, photography, public policy, and recorded music.’",
 			link: "https://tisch.nyu.edu/dance/courses",
 			credit: "",
