07e26ae030ce64046eaf5c04995c4aaaa316206b
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-03-03 12:45:59 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -586,6 +586,14 @@ let WEBSITE_DATA = [
 	},
 	
 
+																										{
+		name: "ROM VS.RAM",
+		img: "images/ScreenShot2023-02-16at8.27.52PM.png.",
+		blurb: "ROM VS. RAM. Memory Present in Computers Humans: (ROM) Read Only Memory; applies to fixed inherent in human behavior. The memory does not depend on an electric current to save data, instead, data is written to individual cells using binary code. (RAM) Reads and Writes Memory; Random Access Memory applies to memory learned from experiences. The information is temporarily stored. It erases when you restart or shut down your computer. Because the information is stored electrically on transistors, when there is no electric current, the data disappears.",
+		link: "",
+		credit: "",
+	},
+	
 	
 	
 	
