c694b27d13435235aae209cd36dea43e7f8754ac
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-03-07 13:41:57 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -373,15 +373,14 @@ let WEBSITE_DATA = [
 	},
 
 	
-	
-	{
-		name: "Hypothalamus",
-		img: "images/ScreenShot2023-02-18at7.34.27PM.png", 
-		blurb: "The hypothalamus is a small area in the center of the brain. It helps produce hormones that regulate heart rate, body temperature, hunger, and the sleep-wake cycle. When the hypothalamus is not working properly, it can cause problems in the body that lead to a wide range of rare disorders. The hypothalamus’ main role is to keep the body in homeostasis as much as possible. Homeostasis means a healthy and balanced internal state. The body is always trying to achieve this balance.",
+
+	 				{
+		name: "Skin Conductance Response",
+		img: "images/ScreenShot2023-02-23at2.05.01PM.png",
+		blurb: "The skin conductance response (SCR) is an indirect measure of sympathetic autonomic activity that is associated with both emotion and attention. In humans, the amplitude of SCRs is related to the level of arousal elicited by visual stimuli with either positive or negative emotional valence.",
 		link: "",
 		credit: "",
 	},
-	
  
 
 	
@@ -471,15 +470,16 @@ let WEBSITE_DATA = [
 	},
 	
 	
-
 	
- 				{
-		name: "Skin Conductance Response",
-		img: "images/ScreenShot2023-02-23at2.05.01PM.png",
-		blurb: "The skin conductance response (SCR) is an indirect measure of sympathetic autonomic activity that is associated with both emotion and attention. In humans, the amplitude of SCRs is related to the level of arousal elicited by visual stimuli with either positive or negative emotional valence.",
+	{
+		name: "Hypothalamus",
+		img: "images/ScreenShot2023-02-18at7.34.27PM.png", 
+		blurb: "The hypothalamus is a small area in the center of the brain. It helps produce hormones that regulate heart rate, body temperature, hunger, and the sleep-wake cycle. When the hypothalamus is not working properly, it can cause problems in the body that lead to a wide range of rare disorders. The hypothalamus’ main role is to keep the body in homeostasis as much as possible. Homeostasis means a healthy and balanced internal state. The body is always trying to achieve this balance.",
 		link: "",
 		credit: "",
 	},
+	
+
 	
 	{
 		name: "Polarization",
