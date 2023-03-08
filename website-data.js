ec51675fb908139efddd44cf609dc634756f5d4b
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-03-07 19:10:24 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -197,7 +197,7 @@ let WEBSITE_DATA = [
 		
 																							{
 		name: "Shower | Performance",
-		img: "images/5S20221028102648PM.jpeg",
+		img: "images/119106655.jpeg",
 		blurb: "’Shower' (2014) a performative work inspired by the music of American musical composer Nico Muhly. Presented at The Peter Jay Sharp Theatre's Symphony Space, New York.",
 		link: "",
 		credit: "",
