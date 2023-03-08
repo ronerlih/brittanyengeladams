c5998700482556c3510f5885c0778228e1b52deb
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-03-08 11:36:30 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -46,7 +46,7 @@ let WEBSITE_DATA = [
 	
 						 												{
 		name: "Autonomous",
-		img: "images/ScreenShot2023-03-02at10.35.19AM.png",
+		img: "images/colosseum_361-1.jpeg",
 		blurb: "Forces of the world allow for a basic interpretations of freewill. In an economic-capitalist sphere freewill represents a sublime force that manifests through desire. This is not freewill. Freewill is not the sublime, sacred idea that pops into your head. It’s not something you have, it’s something struggled for.",
 		link: "",
 		credit: "",
