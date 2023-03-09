2f75f8cc723efe8baa541afed4f462c0b1b421fa
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-03-09 13:51:23 -0500

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -489,7 +489,7 @@ let WEBSITE_DATA = [
 	
 	{
 		name: "Polarization",
-		img: "images/ark_of_the_covenant_01.jpg",
+		img: "images/ScreenShot2023-03-01at9.49.04AM.png",
 		blurb: "Filter Bubbles are an echo chamber, the result of algorithmic dynamics aided by opaque interfaces, caused by the homogenization of friend recommendations and personal news feeds. Where increasingly like minded people whose views are reinforced and become more extreme. In America, political issues have always polarized people. Now more than ever with the proliferation of the internet and online platforms, paradoxically intensifies political ‘homophily.’",
 		link: "",
 		credit: "",
