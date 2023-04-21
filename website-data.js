b59de61b3baa75601e5da305333da880ce1f04f9
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-04-21 17:24:56 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -71,7 +71,7 @@ let WEBSITE_DATA = [
 
 				{
 		name: "Galileo's Law of Falling Bodies",
-		img: "images/fascia__1298.png",
+		img: "images/ark_of_the_covenant_01.jpg",
 		blurb: " Galileo's law of fall claims that bodies fall at a constant acceleration, i.e., that their speed increases by equal increments within equal time periods, and that the distance traveled by them in equal time periods is not equal.",
 		link: "",
 		credit: "",
@@ -106,7 +106,7 @@ let WEBSITE_DATA = [
 	
 		 													{
 		name: "I Am Not A Robot",
-		img: "images/ScreenShot2023-02-18at11.49.32AM.png",
+		img: "images/cyborg_leg.png",
 		blurb: "Adversarial Neural Networks & Cumulative Machine Learning Algorithms. A human performs a series of tasks, as a set of instructions that an avatar simulation is trying to learn and perfect, exploring the interplay between human actions, ecosystems, and the systems that unite them.",
 		link: "",
 		credit: "",
