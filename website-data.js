036af3f4e431e972adf8c2ffc7cec8f3307937a4
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-05-15 22:11:03 -0400

    Update website-data.js

--- website-data.js
+++ website-data.js
@@ -262,7 +262,7 @@ let WEBSITE_DATA = [
 		
 																				{
 		name: "Proprioceptive Research",
-		img: "images/eyeshadow.601.jpg",
+		img: "images/01_v4.jpeg",
 		blurb: "Proprioceptive research using artificial intelligence and motion capture technology. I consider the ways this software could be used for dance training. If avatars could offer multi-dimensional feedback or phrasing, instead of a mirror while training? That feedback could be effective and productive in learning movement, as I'm not relying on ocular relation to create representations, I have to rely on an orientation  to motion and the feeling of being in space. It’s a proprioceptive, tactile, and kinesthetic method.",
 		link: "",
 		credit: "",
