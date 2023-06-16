3f2ccfc88cf3eb3aa6270ecd6c4699fdea229941
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-06-16 16:47:24 -0400

    Update website-data.js
    die

--- website-data.js
+++ website-data.js
@@ -425,7 +425,7 @@ let WEBSITE_DATA = [
 	
 				{
 		name: "Somatosensory Receptors",
-		img: "images/skin-receptors-senses-basics.jpg",
+		img: "images/cyborg_leg.png",
 		blurb: "Somatosensory receptors are specialized sensory nerves that respond to various stimuli, including vision, hearing, taste, and smell, as well as general somatic senses which make up the somatosensory system, which is involved in the sense of touch, proprioception, pain, and temperature.",
 		link: "",
 		credit: "",
