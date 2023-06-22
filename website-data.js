0ba834b433c2b5c7af5afdd586a2418c234f60c3
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-06-22 12:55:18 -0400

    Update website-data.js
    ctt

--- website-data.js
+++ website-data.js
@@ -96,7 +96,7 @@ let WEBSITE_DATA = [
 
 		{
 		name: "Human Robot",
-		img: "images/21519201414113333.gif",
+		img: "images/CTT.png",
 		blurb: "Adversarial Neural Networks & Machine Learning Algorithms. A robot called the 'maquette' which is a prototype. Inspired by Walter Benjamin's “The Work of Art in the Age of Mechanical Reproduction”, and his concept of the ‘aura’, to describe how we use the machine in order to make art, is in a sense a requiem to handmade art. The choreography of the Maquette deals with this chasm between the ‘aura’ of the handmade artwork, and the uniqueness that is deprived through the mechanical reproduction of it.", 
 		link: "",
 		credit: "",
