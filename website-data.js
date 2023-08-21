9d15a897f2a4f36be151014090c513730820c840
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-21 14:08:15 -0400

    Update website-data.js
    edit_omit

--- website-data.js
+++ website-data.js
@@ -28,14 +28,15 @@ let WEBSITE_DATA = [
 	
 },	
 	
-				{
-			
-		name: "Harvestworks Artist Residency",
-		img: "images/ScreenShot20221018at8248M.jpg",
-		blurb: "2023 Artist in Residence at Harvestworks. Harvestworks was founded as a not-for-profit organization by artists in 1977, and has helped a generation of artists create new works using technology. Harvestworks' mission is to support the creation and presentation of art works achieved through the use of new and evolving technologies.",
-		link: "https://www.harvestworks.org/2023-artist-residencies/",
+{
+		name: "BodyMouth Exhibition",
+		img: "images/Futurebodies_by_JoshuaOkoro108.jpg",
+		blurb: "'BodyMouth' presented at The 2022 New Media Caucus 'Future Bodies' Symposium, at Virginia Tech.",
+		link: "",
 		credit: "",
-	},		
+		
+		},		
+		
 {
 	
 		name: "Doppler Effect",
@@ -125,16 +126,15 @@ let WEBSITE_DATA = [
 		link: "",
 		credit: ""
 	},
-
-	
-{
-		name: "BodyMouth Exhibition",
-		img: "images/Futurebodies_by_JoshuaOkoro108.jpg",
-		blurb: "'BodyMouth' presented at The 2022 New Media Caucus 'Future Bodies' Symposium, at Virginia Tech.",
-		link: "",
+				{
+			
+		name: "Harvestworks Artist Residency",
+		img: "images/ScreenShot20221018at8248M.jpg",
+		blurb: "2023 Artist in Residence at Harvestworks. Harvestworks was founded as a not-for-profit organization by artists in 1977, and has helped a generation of artists create new works using technology. Harvestworks' mission is to support the creation and presentation of art works achieved through the use of new and evolving technologies.",
+		link: "https://www.harvestworks.org/2023-artist-residencies/",
 		credit: "",
-		
-		},	
+	},
+
 	
 
 
@@ -491,13 +491,13 @@ let WEBSITE_DATA = [
 	// 	credit: "",
 	// },
 	
-				{
-		name: "Projectile Motion",
-		img: "images/animated-sinewave.gif",
-		blurb: "Projectile motion is the motion of an object thrown or projected into the air, subject only to acceleration as a result of gravity. Such objects are called projectiles and their path is called a trajectory.’",
-		link: "",
-		credit: "",
-	},
+	// 			{
+	// 	name: "Projectile Motion",
+	// 	img: "images/animated-sinewave.gif",
+	// 	blurb: "Projectile motion is the motion of an object thrown or projected into the air, subject only to acceleration as a result of gravity. Such objects are called projectiles and their path is called a trajectory.’",
+	// 	link: "",
+	// 	credit: "",
+	// },
 	
 		
 																										{
