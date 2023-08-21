4921773623d8cc8f73804321eabf3b6d54f29487
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-21 14:19:28 -0400

    Update website-data.js
    aom_edit

--- website-data.js
+++ website-data.js
@@ -147,7 +147,7 @@ let WEBSITE_DATA = [
 		},
 {
 			name: "Architecture of Memory",
-			img: "images/2138383815138158.jpg", 
+			img: "images/snapshot033.png", 
 			blurb: "‘’Architecture of Memory' is a research project that places loss and rupture as opportunity for growth and transformation. Inspired by American Writer Saidiya Hartman's literary device 'Critical Fabulation’ a style of creative semi-nonfiction that attempts to bring the suppressed voices of the past to the surface.",
 			link: "",
 			credit: "",
