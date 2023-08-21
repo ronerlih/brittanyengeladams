f3ba4c1ca35653eb06d132af9a08ae5f2441b867
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-21 11:02:05 -0400

    Update website-data.js
    phot_edit

--- website-data.js
+++ website-data.js
@@ -24,7 +24,7 @@ let WEBSITE_DATA = [
 				{
 			
 		name: "Harvestworks Artist Residency",
-		img: "images/8118221151181119920231518111.jpg",
+		img: "images/ScreenShot20221018at8248M.jpg",
 		blurb: "2023 Artist in Residence at Harvestworks. Harvestworks was founded as a not-for-profit organization by artists in 1977, and has helped a generation of artists create new works using technology. Harvestworks' mission is to support the creation and presentation of art works achieved through the use of new and evolving technologies.",
 		link: "https://www.harvestworks.org/2023-artist-residencies/",
 		credit: "",
