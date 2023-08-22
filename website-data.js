ed51ccd24a359863923f017885f079afed00379f
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-22 14:32:34 -0400

    Update website-data.js
    WF

--- website-data.js
+++ website-data.js
@@ -75,7 +75,7 @@ let WEBSITE_DATA = [
 			{
 		 
 		name: "Wireless Fidelity",
-		img: "images/8537885385349.jpg",
+		img: "images/261949_8111949118111949.png",
 		blurb: "Wireless Fidelity 2015 digital installation at The Gallery Thaddaeus Ropac in Paris France.",
 		link: "",
 	        credit: "",
