588c0abc878cf21c30e34aa7da0867daa82e7a43
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-21 21:48:55 -0400

    Update website-data.js
    edit_omit

--- website-data.js
+++ website-data.js
@@ -337,13 +337,13 @@ let WEBSITE_DATA = [
 
 
 
-			{
-			name: "Cape Cod Modern House Trust Artist Residency (2020-2021)",
-			img: "images/capecodmodernhousetrust.jpg",
-			blurb: "The Cape Cod Modern House Trust (CCMHT) was founded to collect, archive, and share documentation of the Outer Cape’s exceptional modern architecture, restore a group of important, endangered modern houses, and relaunch those houses as platforms for new creative work. Brittany Engel-Adams presented two pieces that she worked on while in Wellfleet, ‘Architecture of Memory’ and ‘Hand Dance’, a dance film commissioned by the Guggenheim Works & Process. The four artists from the 2020 Cape Cod Modern House Trust Residency have work in CCMHT’s Digital Guestbook.", 
-			link: " https://provincetownindependent.org/arts-minds/2021/02/03/wellfleets-modern-houses-are-still-laboratories-for-collaboration/",
-			credit: "",
-		},
+		// 	{
+		// 	name: "Cape Cod Modern House Trust Artist Residency (2020-2021)",
+		// 	img: "images/capecodmodernhousetrust.jpg",
+		// 	blurb: "The Cape Cod Modern House Trust (CCMHT) was founded to collect, archive, and share documentation of the Outer Cape’s exceptional modern architecture, restore a group of important, endangered modern houses, and relaunch those houses as platforms for new creative work. Brittany Engel-Adams presented two pieces that she worked on while in Wellfleet, ‘Architecture of Memory’ and ‘Hand Dance’, a dance film commissioned by the Guggenheim Works & Process. The four artists from the 2020 Cape Cod Modern House Trust Residency have work in CCMHT’s Digital Guestbook.", 
+		// 	link: " https://provincetownindependent.org/arts-minds/2021/02/03/wellfleets-modern-houses-are-still-laboratories-for-collaboration/",
+		// 	credit: "",
+		// },
 	
 
 	
