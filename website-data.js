7caf556bac733d67197447bb2228d5b6a05ca919
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-22 20:30:29 -0400

    Update website-data.js
    GL

--- website-data.js
+++ website-data.js
@@ -299,13 +299,13 @@ let WEBSITE_DATA = [
 	// },
 
 	
-					{
-		name: "Galileo's Law of Falling Bodies",
-		img: "images/fascia__1298.png",
-		blurb: " Galileo's law of fall claims that bodies fall at a constant acceleration, i.e., that their speed increases by equal increments within equal time periods, and that the distance traveled by them in equal time periods is not equal.",
-		link: "",
-		credit: "",
-	},		
+	// 				{
+	// 	name: "Galileo's Law of Falling Bodies",
+	// 	img: "images/fascia__1298.png",
+	// 	blurb: " Galileo's law of fall claims that bodies fall at a constant acceleration, i.e., that their speed increases by equal increments within equal time periods, and that the distance traveled by them in equal time periods is not equal.",
+	// 	link: "",
+	// 	credit: "",
+	// },		
 	
 
 
