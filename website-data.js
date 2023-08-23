5807c93432b30fb1d9d423ed318d6bee706fdbb8
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-23 11:27:05 -0400

    Update website-data.js
    PH

--- website-data.js
+++ website-data.js
@@ -313,7 +313,7 @@ let WEBSITE_DATA = [
 {
 			
 		name: "Posthumanism",
-		img: "images/ScreenShot2023-02-13at7.24.31PM.png",
+		img: "images/ruins.png",
 		blurb: "What happens when anyone can upgrade their body? What aspects of our humanity will we change? Who will decide who goes forth into our species’ next evolutionary phase, and who gets left behind?",
 		link: "",
 		credit:"",
