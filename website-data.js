ffe64121113fae206539b8069199a185efbc560f
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-22 20:06:16 -0400

    Update website-data.js
    MD

--- website-data.js
+++ website-data.js
@@ -126,7 +126,7 @@ let WEBSITE_DATA = [
 	
 																					{
 		name: "Memory Dance",
-		img: "images/12516151516.gif",
+		img: "images/memory.jpeg",
 		blurb: "‘Memory Dance’ uses infrared sensor of kinect, motion capture technology, and turns the human body into 3d spatial-data, records and loops movements (similar to a musical looper), shows a live camera feed of the recording, and superimposes the movements in the frame of the camera.",
 		link: "",
 		credit: "", 
