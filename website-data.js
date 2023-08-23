97d850f99b50ba0723da9b18a167847781fd6ad0
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-23 11:24:50 -0400

    Update website-data.js
    RR

--- website-data.js
+++ website-data.js
@@ -276,7 +276,7 @@ let WEBSITE_DATA = [
 	
 																										{
 		name: "ROM VS. RAM",
-		img: "images/adolph_men_zel123.png",
+		img: "images/ScreenShot2023-02-06at6.00.43PM.png",
 		blurb: "Read Only Memory, applies to fixed inherent in human behavior. The memory does not depend on an electric current to save data, instead, data is written to individual cells using binary code. Random Access Memory, reads and writes memory. Random Access Memory applies to memory learned from experiences. The information is temporarily stored, it erases when you restart or shut down your computer, because the information is stored electrically on transistors. When there is no electrical current, the data disappears.",
 		link: "",
 		credit: "",
