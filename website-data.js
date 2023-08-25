a00439681f01911d700937012d43a1f2e7c236ed
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-25 18:21:46 -0400

    Update website-data.js
    edits gen

--- website-data.js
+++ website-data.js
@@ -154,7 +154,7 @@ let WEBSITE_DATA = [
 
 					{
 		name: "Haptics",
-		img: "images/Madonna_retina_ish_ingU2alalala.png",
+		img: "images/lemon_rr_00.jpeg",
 		blurb: "Haptics as a technology that transmits tactile information using sensations such as vibration, touch, and force feedback.",
 		link: "",
 		credit: "",
