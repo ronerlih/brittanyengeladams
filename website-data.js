7efade803f9143c9391d4faf0449c278b9e4a40b
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-28 12:06:54 -0400

    Update website-data.js
    edit_cont....

--- website-data.js
+++ website-data.js
@@ -14,7 +14,7 @@ let WEBSITE_DATA = [
 
 			{
 		name: "Movement Research Performance Journal Publication",
-		img: "images/131152251314201853115225131420185111181938131152251314201851181938.jpg",
+		img: "images/13181851195138.gif",
 		blurb: "‘Architecture of Memory’ combines archival research, motion-capture technology, and artificial intelligence as an aperture from which to view haunting intersections between past, present, and future. Publication of ‘Architecture of Memory’ in The Movement Research Performance Journal No. 55.",
 		link: "https://movementresearch.org/publications/performance-journal/issue-55",
 		credit: "",
