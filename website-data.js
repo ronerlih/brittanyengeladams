4e2790e291912c8beae077b132eb9b82e8e499d0
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-28 12:49:25 -0400

    Update website-data.js
    EDIT

--- website-data.js
+++ website-data.js
@@ -269,7 +269,7 @@ let WEBSITE_DATA = [
 		
 																		{
 	      name: "Emergent Landscape I Tessellation",
-		img: "images/redzoom.png",
+		img: "images/1266215159166122591612215916612916.png",
 		blurb:  "Online Tessellation",
 		link: "https://https://www.brittanyengeladams.com/who-wood.html",
 		credit: "",
