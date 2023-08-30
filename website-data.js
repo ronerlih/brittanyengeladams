ffed68d8e1729d60cf14aadb285b1d70cef6b1d8
Bengeladams <58406309+Bengeladams@users.noreply.github.com>
2023-08-30 11:46:08 -0400

    Update website-data.js
    edits...

--- website-data.js
+++ website-data.js
@@ -247,7 +247,7 @@ let WEBSITE_DATA = [
 
 						{
 		name: "Newtonian Physics",
-		img: "images/SS2023-02-07at5.45.35PM.png",
+		img: "images/ScreenShot2023-03-02at10.35.19AM.png",
 		blurb: "Newtonian physics is the description of mechanical events—those that involve forces acting on matter—using the laws of motion and gravitation.",
 		link: "",
 		credit: "",
